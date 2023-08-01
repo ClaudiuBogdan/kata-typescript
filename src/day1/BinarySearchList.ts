311c7e19512128090610e505f36c06b31e5b5e22
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-08-01 14:45:15 +0300

    feat: solve linear search and binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,18 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+  return recursive_binary_search(haystack, 0, haystack.length - 1, needle) }
 
+function recursive_binary_search(arr: number[], leftIndex: number, rightIndex: number, value: number): boolean{
+  if(leftIndex > rightIndex){
+    return false
+  }
+  const middleIndex = leftIndex + Math.floor((rightIndex - leftIndex)/2)
+  if(arr[middleIndex] === value){
+    return true
+  }
+  if(arr[middleIndex] > value){
+    return recursive_binary_search(arr, leftIndex, middleIndex - 1, value)
+  }
+  else{
+    return recursive_binary_search(arr, middleIndex + 1, rightIndex, value)
+  }
 }
