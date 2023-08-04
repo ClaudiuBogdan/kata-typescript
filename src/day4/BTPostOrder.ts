781e60932d5b2a9bf63bdb3f3b03d5bb87e467b9
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-08-04 21:19:10 +0300

    feat(day4): solve post order traversal in BST problem

--- src/day4/BTPostOrder.ts
+++ src/day4/BTPostOrder.ts
@@ -1,3 +1,21 @@
 export default function post_order_search(head: BinaryNode<number>): number[] {
+    return post_order_aux(head);
+}
 
+function post_order_aux(
+    head: BinaryNode<number>,
+    acc: number[] = [],
+): number[] {
+    // 1. Print left branch
+    // 2. Print right branch
+    // 3. Print node value
+
+    if (head.left) {
+        post_order_aux(head.left, acc);
+    }
+    if (head.right) {
+        post_order_aux(head.right, acc);
+    }
+    acc.push(head.value);
+    return acc;
 }
