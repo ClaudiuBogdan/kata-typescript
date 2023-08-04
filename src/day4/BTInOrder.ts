9d85eeeda99573daf096be292c67c945dbe202eb
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-08-04 21:15:12 +0300

    feat(day4): solve in order traversal in BST problem

--- src/day4/BTInOrder.ts
+++ src/day4/BTInOrder.ts
@@ -1,3 +1,17 @@
 export default function in_order_search(head: BinaryNode<number>): number[] {
+    return in_order_aux(head);
+}
 
+function in_order_aux(head: BinaryNode<number>, acc: number[] = []): number[] {
+    // 1. Print left branch
+    // 2. Print node value
+    // 3. Print right branch
+    if (head.left) {
+        in_order_aux(head.left, acc);
+    }
+    acc.push(head.value);
+    if (head.right) {
+        in_order_aux(head.right, acc);
+    }
+    return acc;
 }
