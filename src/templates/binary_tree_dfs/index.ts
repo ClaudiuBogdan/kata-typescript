c1da2bf1a44cfee5cd0e0c81d23487b0e4bc6da3
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-08-08 21:38:05 +0300

    feat(templates): update dfs and bfs functions descriptions

--- src/templates/binary_tree_dfs/index.ts
+++ src/templates/binary_tree_dfs/index.ts
@@ -1,3 +1,11 @@
-export default function depthFirstSearch(head: BinaryNode<number>, needle: number): boolean {
+/**
+ * @function depthFirstSearch - Depth First Search
+ * @description Given a binary tree and a target value, return true if the target is found in the binary tree.
+ * @description Depth First Search technique is defined as a method to traverse a Tree such that the parent node is traversed before any of its children.
+ * @link https://www.geeksforgeeks.org/dfs-traversal-of-a-tree-using-recursion/
+ * @param head
+ * @param targe
+ */
+export default function depthFirstSearch(head: BinaryNode<number>, target: number): boolean {
 
 }
