50e6369da8f69e167d03923b3528fd8e4fa0cf32
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-08-08 22:06:58 +0300

    feat(day7): solve compare binary tree problem

--- src/day7/binary_tree_compare/index.ts
+++ src/day7/binary_tree_compare/index.ts
@@ -1,4 +1,24 @@
 export default function compareBinaryTrees(
     rootA: BinaryNode<number>,
     rootB: BinaryNode<number>,
-): boolean {}
+): boolean {
+    return compare(rootA, rootB);
+}
+
+function compare<T>(
+    rootA?: BinaryNode<T> | null,
+    rootB?: BinaryNode<T> | null,
+): boolean {
+    if (rootA === undefined && rootB === undefined) {
+        return true;
+    }
+    if (rootA === null && rootB === null) {
+        return true;
+    }
+    if (rootA?.value !== rootB?.value) {
+        return false;
+    }
+    return (
+        compare(rootA?.left, rootB?.left) && compare(rootA?.right, rootB?.right)
+    );
+}
