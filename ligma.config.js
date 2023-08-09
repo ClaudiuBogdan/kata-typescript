608b691cfd07d771ea3eb758dcafd7406d8e0ae0
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-08-09 16:34:35 +0300

    refactor: migrate to new kata generation

--- ligma.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  dsa: [
-    "BTPreOrder",
-    "BTInOrder",
-    "BTPostOrder",
-  ],
-};
--- src/utils/tests/index.ts
+++ src/utils/tests/index.ts
@@ -1,3 +1,5 @@
+import { list1, list2, matrix2 } from "./graph";
+import { test_list } from "./listTest";
 import { tree, tree2 } from "./tree";
 
-export { tree, tree2 };
+export { tree, tree2, test_list, list1, list2, matrix2 };
