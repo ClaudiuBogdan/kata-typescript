9f6490d1845dbe572ca11a7c1b0d174a5759c9f4
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-08-15 20:49:14 +0300

    feat(template): update dijkstra algorithm description

--- src/templates/graph/dijkstra_list/index.ts
+++ src/templates/graph/dijkstra_list/index.ts
@@ -1,3 +1,11 @@
-export default function dijkstra_list(source: number, sink: number, arr: WeightedAdjacencyList): number[] {
+/**
+ * @function Dijkstra
+ * @description Dijkstra's algorithm is a graph search algorithm that solves the single-source shortest path problem for a graph with non-negative edge weights, producing a shortest-path tree. This algorithm is used to find the minimum distance from the starting vertex to all other vertices in the given graph.
+ * @link https://www.programiz.com/dsa/dijkstra-algorithm
+ * @param source 
+ * @param sink
+ * @param arr 
+ */
+export default function dijkstra_list(source: number, target: number, arr: WeightedAdjacencyList): number[] {
 
 }
