e0c944469be316c24cf1e64f698b0eab5a5f1453
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-08-23 08:52:28 +0300

    feat(template): update insertion sort template

--- src/templates/sort/insertion_sort/index.ts
+++ src/templates/sort/insertion_sort/index.ts
@@ -1,3 +1,10 @@
-export default function insertion_sort(arr: number[]): void {
-
+/**
+ * @function insertionSort
+ * @description Insertion sort is an algorithm that sorts an array by iterating through it and inserting each element into its correct position in a sorted subarray. It works by comparing each element with the previous elements in the subarray and shifting them to the right until the correct position is found for the current element.
+ * @see https://algorithm-visualizer.org/brute-force/insertion-sort
+ * @see https://www.geeksforgeeks.org/insertion-sort/
+ * @param arr
+ */
+export default function insertionSort(arr: number[]): void {
+  
 }
