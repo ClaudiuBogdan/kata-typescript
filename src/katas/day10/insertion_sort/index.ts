acd6da6a90b7cf713b38a4969ac27587042336b3
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-08-23 08:52:55 +0300

    feat(day10): solve insertion sort problem

---
+++ src/katas/day10/insertion_sort/index.ts
@@ -0,0 +1,15 @@
+/**
+ * @function insertionSort
+ * @description Insertion sort is an algorithm that sorts an array by iterating through it and inserting each element into its correct position in a sorted subarray. It works by comparing each element with the previous elements in the subarray and shifting them to the right until the correct position is found for the current element.
+ * @see https://algorithm-visualizer.org/brute-force/insertion-sort
+ * @see https://www.geeksforgeeks.org/insertion-sort/
+ * @param arr
+ */
+export default function insertionSort(arr: number[]): void {
+    for (let i = 1; i < arr.length; i++) {
+        let unsortedValue = arr[i];
+        for (let j = i - 1; j >= 0 && arr[j] > unsortedValue; j--) {
+            [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        }
+    }
+}
