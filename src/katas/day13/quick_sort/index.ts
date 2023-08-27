fd06908eb35f0a2c3df47e17cbda3817ee050de0
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-08-27 12:32:21 +0300

    feat(day13): solve quick sort problem

--- src/katas/day13/quick_sort/index.ts
+++ src/katas/day13/quick_sort/index.ts
@@ -3,7 +3,7 @@
  * @link https://github.com/TheAlgorithms/TypeScript/blob/aab3f3065ae4e0497acaa2a6212ca1d40dcf2134/sorts/quick_sort.ts
  * @description is an algorithm based on divide and conquer approach in which an array is split into sub-arrays and these sub arrays are recursively sorted to get final array
  * @see [Quick Sort](https://www.javatpoint.com/quick-sort)
- * 
+ *
  * @param {number[]} array
  * @returns {number[]}
  * @complexity_analysis
@@ -18,7 +18,26 @@
  */
 
 const quickSort = (arr: number[]): number[] => {
-
+    return qs(arr, 0, arr.length - 1);
 };
+
+function qs(arr: number[], low: number, high: number) {
+    if (low > high) {
+        return arr;
+    }
+    const pivot = arr[high];
+    let idx = low - 1;
+    for (let i = low; i < high; i++) {
+        if (arr[i] <= pivot) {
+            idx++;
+            [arr[i], arr[idx]] = [arr[idx], arr[i]];
+        }
+    }
+    idx++;
+    [arr[idx], arr[high]] = [arr[high], arr[idx]];
+    qs(arr, low, idx - 1);
+    qs(arr, idx + 1, high);
+    return arr;
+}
 
 export default quickSort;
