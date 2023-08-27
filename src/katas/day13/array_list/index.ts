06ea733c229c4ee359cfad8ca7fb2e30be354c4d
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-08-27 11:34:13 +0300

    feat(day13): solve array list problem

--- src/katas/day13/array_list/index.ts
+++ src/katas/day13/array_list/index.ts
@@ -1,27 +1,100 @@
 export default class ArrayList<T> {
-    public length: number;
+    private _length: number;
+    private _capacity: number;
+    private _arr: T[];
 
-    
+    public get length(): number {
+        return this._length;
+    }
 
-    constructor() {
+    constructor(length = 2 ^ 10) {
+        this._capacity = length;
+        this._length = 0;
+        this._arr = new Array(length);
     }
 
     prepend(item: T): void {
-
-}
+        if (this.isFull()) {
+            this.resize();
+        }
+        const arr = this._arr;
+        for (let i = this.length; i > 0; i--) {
+            [arr[i], arr[i - 1]] = [arr[i - 1], arr[i]];
+        }
+        arr[0] = item;
+        this._length++;
+    }
     insertAt(item: T, idx: number): void {
+        if (idx >= this._capacity) {
+            this.resize(idx + 1);
+        }
+        if (idx >= this.length) {
+            this._length = idx + 1;
+        }
+        this._arr[idx] = item;
+    }
 
-}
     append(item: T): void {
+        if (this.isFull()) {
+            this.resize();
+        }
+        this._arr[this.length] = item;
+        this._length++;
+    }
 
-}
     remove(item: T): T | undefined {
-
-}
+        const arr = this._arr
+        let value: T | undefined = undefined
+        let currIdx = -1
+        while(currIdx < this.length && value !== item){
+            currIdx++
+            value = arr[currIdx]
+        }
+        if(currIdx >= this.length){
+            return undefined
+        }
+        return this.removeAt(currIdx)
+    }
     get(idx: number): T | undefined {
-
-}
+        const index = this.getIndex(idx);
+        if (index === undefined) {
+            return;
+        }
+        return this._arr[index];
+    }
     removeAt(idx: number): T | undefined {
+        const arr = this._arr;
+        const index = this.getIndex(idx);
+        if (index === undefined) {
+            return undefined;
+        }
+        let item: T | undefined = arr[index];
+        for (let i = 0; i < this.length - 1; i++) {
+            if (i >= index) {
+                arr[i] = arr[i + 1];
+            }
+        }
+        this._length--;
+        return item;
+    }
 
-}
+    private isFull(): boolean {
+        return this._length === this._capacity;
+    }
+
+    private resize(newCapacity = this._capacity * 2): void {
+        const newArr: T[] = new Array(newCapacity);
+        for (let i = 0; i < this.length; i++) {
+            newArr[i] = this._arr[i];
+        }
+        this._arr = newArr;
+        this._capacity = newCapacity;
+    }
+
+    private getIndex(idx: number): number | undefined {
+        if (Math.abs(idx) >= this.length) {
+            return undefined;
+        }
+        return idx < 0 ? this.length + idx : idx;
+    }
 }
--- src/katas/day13/array_list/test.ts
+++ src/katas/day13/array_list/test.ts
@@ -1,7 +1,55 @@
 import ArrayList from "./index";
 import { test_list } from "@utils/tests";
 
-test("array-list", function () {
-    const list = new ArrayList<number>(3);
-    test_list(list);
-});
+describe('Array list', () => {
+    test('prepend', ()=>{
+        const arr = new ArrayList()
+        arr.prepend(1)
+        expect(arr.get(0)).toEqual(1)
+        arr.prepend(2)
+        expect(arr.get(0)).toEqual(2)
+    })
+
+    test('insertAt', () => {
+        const arr = new ArrayList();
+        arr.insertAt('three', 3)
+        expect(arr.get(3)).toEqual('three')
+        expect(arr.get(0)).toEqual(undefined)
+        expect(arr.get(1)).toEqual(undefined)
+        expect(arr.get(2)).toEqual(undefined)
+        expect(arr.length).toEqual(4)
+    })
+
+    test('append', () => {
+        const arr = new ArrayList();
+        arr.append(1)
+        expect(arr.length).toEqual(1)
+        expect(arr.get(0)).toEqual(1)
+        arr.append(2)
+        expect(arr.length).toEqual(2)
+        expect(arr.get(1)).toEqual(2)
+
+
+    })
+
+    test('removeAt', ()=> {
+        const arr = new ArrayList();
+        arr.append(1)
+        expect(arr.length).toEqual(1)
+        expect(arr.get(0)).toEqual(1)
+        arr.removeAt(0)
+        expect(arr.length).toEqual(0)
+        expect(arr.get(0)).toEqual(undefined)
+
+        arr.append(1)
+        arr.append(2)
+        arr.removeAt(0)
+        expect(arr.get(0)).toEqual(2)
+        expect(arr.length).toEqual(1)
+    })
+
+    test("array-list", function () {
+        const list = new ArrayList<number>(3);
+        test_list(list);
+    });    
+})
