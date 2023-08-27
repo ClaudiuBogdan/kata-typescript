5838c749c5ac4a8c6e2504b0a4b2e1ac8300541f
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-08-27 14:10:43 +0300

    feat(day13): solve two crystal balls problem

--- src/katas/day13/two_crystals_balls/index.ts
+++ src/katas/day13/two_crystals_balls/index.ts
@@ -1,3 +1,20 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
-
+    const steps = Math.floor(Math.sqrt(breaks.length));
+    let currStep = steps;
+    for (; currStep < breaks.length; currStep += steps) {
+        if (breaks[currStep]) {
+            break;
+        }
+    }
+    if (!breaks[currStep]) {
+        return -1;
+    }
+    const prevStep = currStep - steps;
+    let i = prevStep;
+    for (; i <= currStep; i++) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+    return i;
 }
