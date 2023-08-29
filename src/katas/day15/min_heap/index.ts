f2a3530cfbd992f268f500ace77826fa12dfb73d
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-08-29 10:20:27 +0300

    feat(day15): solve min heap problem

--- src/katas/day15/min_heap/index.ts
+++ src/katas/day15/min_heap/index.ts
@@ -6,14 +6,73 @@ import { IHeap } from "./types";
  * @link - https://www.geeksforgeeks.org/heap-data-structure/
  */
 export default class MinHeap<T> implements IHeap<T> {
-    readonly size: number;
+    private _size: number;
+    private _heap: T[];
+
+    public get size(): number {
+        return this._size;
+    }
+
+    constructor() {
+        this._size = 0;
+        this._heap = [];
+    }
+
     insert(value: T): void {
-        throw new Error("Method not implemented.");
+        this._heap.push(value);
+        this._size++;
+        this.bubbleUp(this.size - 1);
     }
     peek(): T | undefined {
-        throw new Error("Method not implemented.");
+        return this._heap[0];
     }
     pop(): T | undefined {
-        throw new Error("Method not implemented.");
+        const minNode = this.peek();
+        this._heap[0] = this._heap[this.size - 1];
+        this._size--;
+        this.bubbleDown(0);
+        return minNode;
+    }
+
+    private bubbleUp(idx: number): void {
+        if (idx <= 0 || idx >= this.size) {
+            return;
+        }
+        const parentIdx = this.getParentIdx(idx);
+        if (this._heap[parentIdx] > this._heap[idx]) {
+            [this._heap[parentIdx], this._heap[idx]] = [
+                this._heap[idx],
+                this._heap[parentIdx],
+            ];
+            this.bubbleUp(parentIdx);
+        }
+    }
+
+    private bubbleDown(idx: number): void {
+        if (idx < 0 || idx >= this.size - 1) {
+            return;
+        }
+        const leftIdx = idx * 2 + 1;
+        const rightIdx = idx * 2 + 2;
+        const minIdx =
+            rightIdx < this.size && this._heap[rightIdx] < this._heap[leftIdx]
+                ? rightIdx
+                : leftIdx;
+        if (this._heap[idx] > this._heap[minIdx]) {
+            [this._heap[idx], this._heap[minIdx]] = [
+                this._heap[minIdx],
+                this._heap[idx],
+            ];
+            this.bubbleDown(minIdx);
+        }
+    }
+
+    private getParentIdx(idx: number): number {
+        if (idx <= 0 || idx >= this.size) {
+            throw new Error(
+                `Invalid index ${idx}. Parent index cannot be calculated`,
+            );
+        }
+        return Math.floor((idx - 1) / 2);
     }
 }
--- src/katas/day15/min_heap/test.ts
+++ src/katas/day15/min_heap/test.ts
@@ -1,29 +1,59 @@
 import MinHeap from "./index";
 
-test("min heap", function () {
-    const heap = new MinHeap<number>();
+describe('Min heap', () => {
+    test('min heap with same left and right values', () => {
+        const heap = new MinHeap<number>();
+        heap.insert(1)
+        heap.insert(2)
+        heap.insert(2)
+        heap.insert(3)
+        heap.insert(4)
+        expect(heap.pop()).toEqual(1)
+        expect(heap.pop()).toEqual(2)
+        expect(heap.pop()).toEqual(2)
+        expect(heap.pop()).toEqual(3)
+        expect(heap.pop()).toEqual(4)
 
-    expect(heap.size).toEqual(0);
+        const heap2 = new MinHeap<number>();
+        heap2.insert(1)
+        heap2.insert(2)
+        heap2.insert(2)
+        heap2.insert(4)
+        heap2.insert(3)
 
-    heap.insert(5);
-    heap.insert(3);
-    heap.insert(69);
-    heap.insert(420);
-    heap.insert(4);
-    heap.insert(1);
-    heap.insert(8);
-    heap.insert(7);
 
-    expect(heap.size).toEqual(8);
-    expect(heap.pop()).toEqual(1);
-    expect(heap.pop()).toEqual(3);
-    expect(heap.pop()).toEqual(4);
-    expect(heap.pop()).toEqual(5);
-    expect(heap.size).toEqual(4);
-    expect(heap.peek()).toEqual(7);
-    expect(heap.pop()).toEqual(7);
-    expect(heap.pop()).toEqual(8);
-    expect(heap.pop()).toEqual(69);
-    expect(heap.pop()).toEqual(420);
-    expect(heap.size).toEqual(0);
-});
+        expect(heap2.pop()).toEqual(1)
+        expect(heap2.pop()).toEqual(2)
+        expect(heap2.pop()).toEqual(2)
+        expect(heap2.pop()).toEqual(3)
+        expect(heap2.pop()).toEqual(4)
+    })
+    test("min heap", function () {
+        const heap = new MinHeap<number>();
+    
+        expect(heap.size).toEqual(0);
+    
+        heap.insert(5);
+        heap.insert(3);
+        heap.insert(69);
+        heap.insert(420);
+        heap.insert(4);
+        heap.insert(1);
+        heap.insert(8);
+        heap.insert(7);
+    
+        expect(heap.size).toEqual(8);
+        expect(heap.pop()).toEqual(1);
+        expect(heap.pop()).toEqual(3);
+        expect(heap.pop()).toEqual(4);
+        expect(heap.pop()).toEqual(5);
+        expect(heap.size).toEqual(4);
+        expect(heap.peek()).toEqual(7);
+        expect(heap.pop()).toEqual(7);
+        expect(heap.pop()).toEqual(8);
+        expect(heap.pop()).toEqual(69);
+        expect(heap.pop()).toEqual(420);
+        expect(heap.size).toEqual(0);
+    });
+    
+})
