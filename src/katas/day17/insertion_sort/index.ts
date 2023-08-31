2307b07879f68e16be75777b4292616231979bea
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-08-31 11:30:32 +0300

    feat(day17): solve insertion sort problem

--- src/katas/day17/insertion_sort/index.ts
+++ src/katas/day17/insertion_sort/index.ts
@@ -6,5 +6,13 @@
  * @param arr
  */
 export default function insertionSort(arr: number[]): void {
-  
+    for (let i = 1; i < arr.length; i++) {
+        for (let j = i; j > 0; j--) {
+            if (arr[j - 1] > arr[j]) {
+                [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]];
+            } else {
+                break;
+            }
+        }
+    }
 }
