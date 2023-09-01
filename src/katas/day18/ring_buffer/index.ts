f1f80575a7a3d5c25f14714c0f1b348b930f2b7f
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-09-01 10:08:21 +0300

    feat(day18): solve ring buffer problem

--- src/katas/day18/ring_buffer/index.ts
+++ src/katas/day18/ring_buffer/index.ts
@@ -1,3 +1,62 @@
 export default class RingBuffer<T> implements IRingBuffer<T> {
-    
+    private capacity: number;
+    private buff: T[];
+    private head: number;
+    private tail: number;
+    private empty: boolean;
+    private full: boolean;
+
+    constructor(capacity: number) {
+        if (capacity < 1) {
+            throw new Error("Capacity must be greater than 1.");
+        }
+        this.capacity = capacity;
+        this.buff = [];
+        this.head = -1;
+        this.tail = 0;
+        this.empty = true;
+        this.full = false;
+    }
+
+    enqueue(item: T): void {
+        if (this.full) {
+            throw new Error("Buffer is full.");
+        }
+        const nextPos = this.getNextPost(this.head);
+        this.buff[nextPos] = item;
+        this.head = nextPos;
+        this.full = this.getNextPost(this.head) === this.tail;
+        this.empty = false;
+    }
+
+    dequeue(): T | undefined {
+        if (this.empty) {
+            return undefined;
+        }
+        const item = this.buff[this.tail];
+        const nextPost = this.getNextPost(this.tail);
+        this.tail = nextPost;
+        this.empty = this.getNextPost(this.head) === this.tail;
+        this.full = false;
+        return item;
+    }
+
+    peak(): T | undefined {
+        if (this.empty) {
+            return undefined;
+        }
+        return this.buff[this.head];
+    }
+
+    isEmpty(): boolean {
+        return this.empty;
+    }
+
+    isFull(): boolean {
+        return this.full;
+    }
+
+    private getNextPost(idx: number): number {
+        return idx === this.capacity - 1 ? 0 : idx + 1;
+    }
 }
