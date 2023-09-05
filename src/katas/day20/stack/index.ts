2f7c367024e25c3d018e300444f87eece0ed50ef
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-09-05 11:37:04 +0300

    feat(day20): solve stack problem

--- src/katas/day20/stack/index.ts
+++ src/katas/day20/stack/index.ts
@@ -1,18 +1,48 @@
 export default class Stack<T> {
-    public length: number;
+    private _tail: ListNode<T> | undefined;
+    private _length: number;
 
-    
+    public get length(): number {
+        return this._length;
+    }
 
     constructor() {
+        this._length = 0;
+        this._tail = undefined;
     }
 
     push(item: T): void {
+        const node = this.createNode(item);
+        this.linkNodes(this._tail, node);
+        this._tail = node;
+        this._length++;
+    }
 
-}
     pop(): T | undefined {
-
-}
+        if (this.length === 0) {
+            return;
+        }
+        const node = this._tail;
+        this._tail = this._tail?.prev;
+        this._length--;
+        return node?.value;
+    }
     peek(): T | undefined {
+        return this._tail?.value;
+    }
 
-}
+    private createNode(item: T): ListNode<T> {
+        return {
+            value: item,
+        };
+    }
+
+    private linkNodes(nodeA?: ListNode<T>, nodeB?: ListNode<T>): void {
+        if (nodeA) {
+            nodeA.next = nodeB;
+        }
+        if (nodeB) {
+            nodeB.prev = nodeA;
+        }
+    }
 }
