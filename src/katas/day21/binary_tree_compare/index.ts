ee278235a066072347098803fdd2a70c488cbdd2
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-09-06 08:55:52 +0300

    feat(day21): solve bt compare problem

--- src/katas/day21/binary_tree_compare/index.ts
+++ src/katas/day21/binary_tree_compare/index.ts
@@ -1,4 +1,25 @@
 export default function compareBinaryTrees(
     rootA: BinaryNode<number>,
     rootB: BinaryNode<number>,
-): boolean {}
+): boolean {
+    const compare = (
+        rootA: BinaryNode<number> | null,
+        rootB: BinaryNode<number> | null,
+    ): boolean => {
+        if (!rootA && !rootB) {
+            return true;
+        }
+        if (!rootA && !!rootB) {
+            return false;
+        }
+        if (!!rootA && !rootB) {
+            return false;
+        }
+        return (
+            rootA?.value === rootB?.value &&
+            compare(rootA!.left, rootB!.left) &&
+            compare(rootA!.right, rootB!.right)
+        );
+    };
+    return compare(rootA, rootB);
+}
