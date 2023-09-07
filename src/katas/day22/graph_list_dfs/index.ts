2bc646200f2e08d784ccf25a1f6ca2ba926b97c0
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-09-07 08:56:27 +0300

    feat(day22): solve graph dfs problem

--- src/katas/day22/graph_list_dfs/index.ts
+++ src/katas/day22/graph_list_dfs/index.ts
@@ -1,11 +1,38 @@
 /**
  * @function DepthFirstSearch - Depth first search on graph
- * @description 
+ * @description
  * @link https://www.geeksforgeeks.org/depth-first-search-or-dfs-for-a-graph/
  * @param graph WeightedAdjacencyList
- * @param source 
- * @param needle 
+ * @param source
+ * @param needle
  */
-export default function dfs(graph: WeightedAdjacencyList, source: number, target: number): number[] | null {
+export default function dfs(
+    graph: WeightedAdjacencyList,
+    source: number,
+    target: number,
+): number[] | null {
+    const seen: boolean[] = [];
+    const path: number[] = [];
 
+    const search = (node: number): boolean => {
+        if (node === target) {
+            path.push(node);
+            return true;
+        }
+        seen[node] = true;
+        for (let edge of graph[node]) {
+            if (seen[edge.to]) {
+                continue;
+            }
+            const found = search(edge.to);
+            if (found) {
+                path.push(node);
+                return true;
+            }
+        }
+        return false;
+    };
+
+    search(source);
+    return path.length === 0 ? null : path.reverse();
 }
