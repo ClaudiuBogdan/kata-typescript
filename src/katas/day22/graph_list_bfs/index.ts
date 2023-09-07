656c575ab44cf40fc986720917915cb32b8c5de3
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-09-07 08:44:28 +0300

    feat(day22): solve graph bfs problem

--- src/katas/day22/graph_list_bfs/index.ts
+++ src/katas/day22/graph_list_bfs/index.ts
@@ -1,10 +1,44 @@
 /**
  * @function dfs - Breadth first search
  * @description Searches for a path from source to target in a graph. Breadth first search means that we search the graph layer by layer, starting from the source adding the next layer of nodes to the queue.
- * @param graph 
- * @param source 
- * @param target 
+ * @param graph
+ * @param source
+ * @param target
  */
-export default function bfs(graph: WeightedAdjacencyList, source: number, target: number): number[] | null {
-  
+export default function bfs(
+    graph: WeightedAdjacencyList,
+    source: number,
+    target: number,
+): number[] | null {
+    const q: number[] = [source];
+    const seen: boolean[] = new Array(graph.length).fill(false);
+    const prev: number[] = new Array(graph.length).fill(-1);
+    while (q.length > 0) {
+        const node = q.shift() as number;
+        seen[node] = true;
+        if (node === target) {
+            return getPath(node, prev);
+        }
+        for (let edge of graph[node]) {
+            if (seen[edge.to]) {
+                continue;
+            }
+            prev[edge.to] = node;
+            q.push(edge.to);
+        }
+    }
+    return null;
+}
+
+function getPath(initialNode: number, prev: number[]): number[] {
+    const path: number[] = [];
+    let node = initialNode;
+    for (let i = 0; i <= prev.length; i++) {
+        if (node === -1) {
+            return path.reverse();
+        }
+        path.push(node);
+        node = prev[node];
+    }
+    throw new Error("Invalid prev array.");
 }
