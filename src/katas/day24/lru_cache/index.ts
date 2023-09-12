263ce440de9c967bbf5414ea438fd916724a72b6
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-09-12 08:22:18 +0300

    feat(day24): generate day 24

---
+++ src/katas/day24/lru_cache/index.ts
@@ -0,0 +1,20 @@
+/**
+ * @class LRU
+ * @description LRU stands for "Least Recently Used" and is a caching algorithm used to manage memory. The LRU cache stores a limited number of items and discards the least recently used item when the cache is full and a new item is added. The LRU class is a TypeScript implementation of this algorithm, which can be used to manage a cache of key-value pairs.
+ * @link https://www.geeksforgeeks.org/lru-cache-implementation/
+ */
+export default class LRU<K, V> {
+    private length: number;
+
+    constructor() {
+        
+    }
+
+    update(key: K, value: V): void {
+
+    }
+
+    get(key: K): V | undefined {
+        
+    }
+}
