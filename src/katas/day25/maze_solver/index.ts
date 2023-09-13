20a82c88e3d51061910db7ac94c3045ebdcb13d9
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-09-13 11:35:05 +0300

    feat(day25): solve maze problem

--- src/katas/day25/maze_solver/index.ts
+++ src/katas/day25/maze_solver/index.ts
@@ -1,3 +1,45 @@
-export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
+const directions = [
+    [0, 1],
+    [1, 0],
+    [0, -1],
+    [-1, 0],
+];
 
+export default function solve(
+    maze: string[],
+    wall: string,
+    start: Point,
+    end: Point,
+): Point[] {
+    const seen: boolean[][] = Array.from({ length: maze.length }, () =>
+        new Array(maze[0].length).fill(false),
+    );
+    const path: Point[] = [];
+    const findPath = (pos: Point): boolean => {
+        seen[pos.y][pos.x] = true;
+        if (pos.x === end.x && pos.y === end.y) {
+            path.push(pos);
+            return true;
+        }
+        for (let [dirX, dirY] of directions) {
+            const nextPost = {
+                x: pos.x + dirX,
+                y: pos.y + dirY,
+            };
+            if (
+                seen[nextPost.y][nextPost.x] ||
+                maze[nextPost.y][nextPost.x] === wall
+            ) {
+                continue;
+            }
+            const found = findPath(nextPost);
+            if (found) {
+                path.push(pos);
+                return true; // TODO: return true when the position is found
+            }
+        }
+        return false;
+    };
+    findPath(start);
+    return path;
 }
