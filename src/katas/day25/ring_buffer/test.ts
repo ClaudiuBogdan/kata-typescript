8bd494963cb2dc57bf9f0ab80e2b401af109c238
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-09-13 11:06:46 +0300

    feat(day25): generate day 25

---
+++ src/katas/day25/ring_buffer/test.ts
@@ -0,0 +1,28 @@
+import RingBuffer from "./index";
+
+test("RingBuffer", function () {
+    const buffer: IRingBuffer<number> = new RingBuffer<number>(3);
+
+    buffer.enqueue(5);
+    expect(buffer.dequeue()).toEqual(5);
+    expect(buffer.dequeue()).toEqual(undefined);
+
+    buffer.enqueue(42);
+    buffer.enqueue(9);
+    expect(buffer.isEmpty()).toEqual(false);
+    expect(buffer.dequeue()).toEqual(42);
+    expect(buffer.dequeue()).toEqual(9);
+    expect(buffer.dequeue()).toEqual(undefined);
+    expect(buffer.isEmpty()).toEqual(true);
+
+    buffer.enqueue(42);
+    expect(buffer.peak()).toEqual(42);
+    expect(buffer.dequeue()).toEqual(42);
+
+    expect(buffer.isEmpty()).toEqual(true);
+    expect(buffer.isFull()).toEqual(false);
+    buffer.enqueue(43);
+    buffer.enqueue(44);
+    buffer.enqueue(45);
+    expect(buffer.isFull()).toEqual(true);
+});
