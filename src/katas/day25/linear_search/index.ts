36d8716480680f0f9b1cc0db3143e5ffa21f0f50
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-09-13 11:21:09 +0300

    feat(day25): solve linear search problem

--- src/katas/day25/linear_search/index.ts
+++ src/katas/day25/linear_search/index.ts
@@ -12,5 +12,11 @@
  * @example linearSearch([1,5,6], 2) => -1
  */
 export default function linearSearch(arr: number[], target: number): number {
-
+    for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
+        if (element === target) {
+            return i;
+        }
+    }
+    return -1;
 }
