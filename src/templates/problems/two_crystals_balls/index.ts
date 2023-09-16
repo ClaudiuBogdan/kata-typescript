8d37525d5c0aa12abef0376d4022a727c16be03e
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-09-16 08:36:06 +0300

    feat(template): add link with solution to two crystal balls

--- src/templates/problems/two_crystals_balls/index.ts
+++ src/templates/problems/two_crystals_balls/index.ts
@@ -1,3 +1,7 @@
+/**
+ * @link https://chat.openai.com/share/6df8adc2-c6a4-4aad-8ac8-4f401aa2b926
+ * @param breaks
+ */
 export default function two_crystal_balls(breaks: boolean[]): number {
-
+  
 }
