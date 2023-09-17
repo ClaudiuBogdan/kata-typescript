673186f4bde059896e9a3bff3495902fb754295e
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-09-17 11:36:33 +0300

    feat(day29): generate day 29

---
+++ src/katas/day29/binary_tree_bfs/test.ts
@@ -0,0 +1,12 @@
+import { tree } from "@utils/tests";
+import bfs from "./index";
+
+test("bt bfs", function () {
+    expect(bfs(tree, 45)).toEqual(true);
+    expect(bfs(tree, 7)).toEqual(true);
+    expect(bfs(tree, 69)).toEqual(false);
+});
+
+
+
+
