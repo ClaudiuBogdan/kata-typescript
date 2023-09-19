5c99392a4ae00312e971b6f30e752462b4071b89
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-09-19 14:58:06 +0300

    feat(day31): add chat-gpt solution for bt search

--- src/katas/day31/binary_tree_search_operations/index.ts
+++ src/katas/day31/binary_tree_search_operations/index.ts
@@ -5,7 +5,7 @@ export class TreeNode {
     val: number;
     left: TreeNode | null;
     right: TreeNode | null;
-    constructor(val: number, left?: TreeNode | null, right?: TreeNode | null) {
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
         this.val = val === undefined ? 0 : val;
         this.left = left === undefined ? null : left;
         this.right = right === undefined ? null : right;
@@ -27,25 +27,21 @@ export class BST {
      * @param {number} val - The value to insert.
      */
     insert(val: number): void {
-        const node = new TreeNode(val);
-        if (!this.root) {
-            this.root = node;
-            return;
+        this.root = this._insert(this.root, val);
+    }
+
+    private _insert(node: TreeNode | null, val: number): TreeNode {
+        if (node === null) {
+            return new TreeNode(val);
         }
-        const insert = (parentNode: TreeNode, node: TreeNode): void => {
-            if (node.val <= parentNode.val && parentNode.left !== null) {
-                return insert(parentNode.left, node);
-            }
-            if (node.val <= parentNode.val) {
-                parentNode.left = node;
-                return;
-            }
-            if (parentNode.right) {
-                return insert(parentNode.right, node);
-            }
-            parentNode.right = node;
-        };
-        return insert(this.root, node);
+
+        if (val <= node.val) {
+            node.left = this._insert(node.left, val);
+        } else if (val > node.val) {
+            node.right = this._insert(node.right, val);
+        }
+
+        return node;
     }
 
     /**
@@ -54,16 +50,21 @@ export class BST {
      * @returns {boolean} True if the value is found, otherwise false.
      */
     search(val: number): boolean {
-        const search = (root: TreeNode | null): boolean => {
-            if (!root) {
-                return false;
-            }
-            if (root.val === val) {
-                return true;
-            }
-            return search(root.left) || search(root.right);
-        };
-        return search(this.root);
+        return this._search(this.root, val);
+    }
+
+    private _search(node: TreeNode | null, val: number): boolean {
+        if (node === null) {
+            return false;
+        }
+
+        if (val === node.val) {
+            return true;
+        } else if (val < node.val) {
+            return this._search(node.left, val);
+        } else {
+            return this._search(node.right, val);
+        }
     }
 
     /**
@@ -71,93 +72,37 @@ export class BST {
      * @param {number} val - The value to delete.
      */
     delete(val: number): void {
-        if (this.root === null) {
-            return;
-        }
-        const [node, parentNode] = this.findNode(val);
-        if (!node) {
-            return;
-        }
-        const [leftMinNode, parentLeftMinNode] = this.findMinNode(node.left);
-        const isRoot = this.root === node;
-        if (leftMinNode) {
-            this.removeNode(leftMinNode, parentLeftMinNode ?? node);
-            this.switchNodes(node, leftMinNode, parentNode, isRoot);
-            return;
-        }
-        const [rightMinNode, parentRightMinNode] = this.findMinNode(node.right);
-        if (rightMinNode) {
-            this.removeNode(rightMinNode, parentRightMinNode ?? node);
-            this.switchNodes(node, rightMinNode, parentNode, isRoot);
-            return;
-        }
-        this.switchNodes(node, null, parentNode, isRoot);
+        this.root = this._delete(this.root, val);
     }
 
-    private findNode(val: number): [TreeNode | null, TreeNode | null] {
-        let parentNode = this.root;
-        const search = (node: TreeNode | null): TreeNode | null => {
-            if (!node) {
-                return null;
+    private _delete(node: TreeNode | null, val: number): TreeNode | null {
+        if (node === null) {
+            return null;
+        }
+
+        if (val === node.val) {
+            if (node.left === null) {
+                return node.right;
             }
-            if (node.val === val) {
-                return node;
+            if (node.right === null) {
+                return node.left;
             }
-            parentNode = node;
-            return search(node.left) || search(node.right);
-        };
-        const node = search(parentNode);
-        return [node, parentNode];
+            let minLargerNode = this._getMin(node.right);
+            node.val = minLargerNode.val;
+            node.right = this._delete(node.right, minLargerNode.val);
+        } else if (val < node.val) {
+            node.left = this._delete(node.left, val);
+        } else {
+            node.right = this._delete(node.right, val);
+        }
+
+        return node;
     }
 
-    private findMinNode(
-        root: TreeNode | null,
-    ): [TreeNode | null, TreeNode | null] {
-        if (root === null) {
-            return [null, null];
+    private _getMin(node: TreeNode): TreeNode {
+        while (node.left !== null) {
+            node = node.left;
         }
-        let parent: TreeNode | null = root;
-        const findMin = (node: TreeNode | null): TreeNode | null => {
-            if (!node) {
-                return null;
-            }
-            if (node.left) {
-                parent = node;
-                return findMin(node.left);
-            }
-            return node;
-        };
-        const minNode = findMin(parent);
-        parent = parent === minNode ? null : parent;
-        return [minNode, parent];
-    }
-
-    private removeNode(node: TreeNode, parent: TreeNode): void {
-        if (parent.left === node) {
-            parent.left = null;
-        }
-        if (parent.right === node) {
-            parent.right = null;
-        }
-    }
-
-    private switchNodes(
-        node: TreeNode,
-        newNode: TreeNode | null,
-        parentNode: TreeNode | null,
-        isRoot = false,
-    ): void {
-        if (isRoot) {
-            this.root = newNode;
-        }
-        if (!parentNode) {
-            return;
-        }
-        if (parentNode.left === node) {
-            parentNode.left = newNode;
-        }
-        if (parentNode.right === node) {
-            parentNode.right = newNode;
-        }
+        return node;
     }
 }
