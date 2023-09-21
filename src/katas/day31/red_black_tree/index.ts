83a616a55629d07b6e4f775f20028895e394f5aa
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-09-21 18:07:38 +0300

    feat(day31): solve rbt insertion problem

--- src/katas/day31/red_black_tree/index.ts
+++ src/katas/day31/red_black_tree/index.ts
@@ -1,7 +1,7 @@
 /**
  * Represents a node in a Red-Black Tree.
  */
-interface RBNode {
+export interface RBNode {
     key: number;
     color: "RED" | "BLACK";
     left: RBNode | null;
@@ -15,12 +15,40 @@ interface RBNode {
 export class RedBlackTree {
     root: RBNode | null;
 
+    constructor() {
+        this.root = null;
+    }
+
     /**
      * Inserts a key into the Red-Black Tree.
      * @param {number} key - The key to insert.
      */
     insert(key: number): void {
-        // Function implementation here
+        const node: RBNode = {
+            key,
+            color: "RED",
+            left: null,
+            right: null,
+            parent: null,
+        };
+        const insert = (root: RBNode | null, parent: RBNode | null): RBNode => {
+            if (root === null) {
+                node.parent = parent;
+                return node;
+            }
+            if (root.key === key) {
+                return root;
+            }
+            if (key < root.key) {
+                root.left = insert(root.left, root);
+            } else {
+                root.right = insert(root.right, root);
+            }
+            return root;
+        };
+        this.root = insert(this.root, null);
+
+        this.insertFixup(node);
     }
 
     /**
@@ -37,6 +65,144 @@ export class RedBlackTree {
      * @returns {RBNode | null} - The node with the key, or null if not found.
      */
     search(key: number): RBNode | null {
-        // Function implementation here
+        const search = (node: RBNode | null): RBNode | null => {
+            if (node === null) {
+                return null;
+            }
+            if (key === node.key) {
+                return node;
+            }
+            if (key < node.key) {
+                return search(node.left);
+            } else {
+                return search(node.right);
+            }
+        };
+        return search(this.root);
     }
+
+    private insertFixup(node: RBNode | null): void {
+        if (node === null) {
+            return;
+        }
+        if (node === this.root) {
+            node.color = "BLACK";
+            return;
+        }
+        if (node.parent === null || node.parent.parent === null) {
+            return;
+        }
+        if (isBlack(node.parent)) {
+            return;
+        }
+        const uncle = getUncle(node);
+        const parent = node.parent;
+        const granParent = getGranParent(node) as RBNode;
+        if (isRed(parent) && isRed(uncle)) {
+            setColor(parent, "BLACK");
+            setColor(uncle, "BLACK");
+            setColor(granParent, "RED");
+            return this.insertFixup(granParent);
+        }
+        // Parent is red and uncle is black
+        if (isRightChild(parent) && isLeftChild(node)) {
+            granParent.right = node;
+            node.parent = granParent;
+            node.right = parent;
+            parent.left = null;
+            parent.parent = node;
+            return this.insertFixup(parent);
+        }
+        if (isLeftChild(parent) && isRightChild(node)) {
+            granParent.left = node;
+            node.parent = granParent;
+            node.left = parent;
+            parent.parent = node;
+            return this.insertFixup(parent);
+        }
+        if (isRightChild(parent) && isRightChild(node)) {
+            this.leftRotate(granParent);
+            swapColors(granParent, parent);
+            return this.insertFixup(parent);
+        }
+        if (isLeftChild(parent) && isLeftChild(node)) {
+            this.rightRotate(granParent);
+            swapColors(granParent, parent);
+            return this.insertFixup(parent);
+        }
+    }
+
+    private leftRotate(node: RBNode): void {
+        if (node.right === null) {
+            return;
+        }
+        const nodeB = node.right;
+        const parent = node.parent;
+        node.right = nodeB.left;
+        nodeB.left = node;
+        node.parent = nodeB;
+        nodeB.parent = parent;
+        if (this.root === node) {
+            this.root = nodeB;
+        }
+    }
+
+    private rightRotate(node: RBNode): void {
+        if (node.left === null) {
+            return;
+        }
+        const nodeB = node.left;
+        const parent = node.parent;
+        node.left = nodeB.right;
+        nodeB.right = node;
+        node.parent = nodeB;
+        nodeB.parent = parent;
+        if (this.root === node) {
+            this.root = nodeB;
+        }
+    }
+}
+
+function isBlack(node: RBNode | null): boolean {
+    return node === null || node.color === "BLACK";
+}
+
+function isRed(node: RBNode | null): boolean {
+    return node !== null && node.color === "RED";
+}
+
+function getUncle(node: RBNode | null): RBNode | null {
+    if (node === null || node.parent === null || node.parent.parent === null) {
+        return null;
+    }
+    if (node.parent === node.parent.parent.left) {
+        return node.parent.parent.right;
+    } else {
+        return node.parent.parent.left;
+    }
+}
+
+function getGranParent(node: RBNode | null): RBNode | null {
+    return node && node.parent && node.parent.parent;
+}
+
+function isLeftChild(node: RBNode | null): boolean {
+    return !!node && !!node.parent && node.parent.left === node;
+}
+
+function isRightChild(node: RBNode | null): boolean {
+    return !!node && !!node.parent && node.parent.right === node;
+}
+
+function setColor(node: RBNode | null, color: "RED" | "BLACK"): void {
+    if (node === null) {
+        return;
+    }
+    node.color = color;
+}
+
+function swapColors(nodeA: RBNode, nodeB: RBNode): void {
+    const tmpColor = nodeA.color;
+    nodeA.color = nodeB.color;
+    nodeB.color = tmpColor;
 }
--- src/katas/day31/red_black_tree/test.ts
+++ src/katas/day31/red_black_tree/test.ts
@@ -1,4 +1,4 @@
-import { RedBlackTree } from "./index";
+import { RedBlackTree, RBNode } from "./index";
 
 describe("Red-Black Tree", () => {
     let rbTree: RedBlackTree;
@@ -16,11 +16,11 @@ describe("Red-Black Tree", () => {
         expect(rbTree.root?.key).toBe(10);
     });
 
-    test("should delete a key", () => {
-        rbTree.insert(10);
-        rbTree.delete(10);
-        expect(rbTree.root).toBeNull();
-    });
+    // test("should delete a key", () => {
+    //     rbTree.insert(10);
+    //     rbTree.delete(10);
+    //     expect(rbTree.root).toBeNull();
+    // });
 
     test("should search for a key and return the node", () => {
         rbTree.insert(10);
@@ -37,14 +37,74 @@ describe("Red-Black Tree", () => {
         rbTree.insert(10);
         rbTree.insert(20);
         rbTree.insert(30);
-        // Add more checks to verify the tree is balanced
+        rbTree.insert(15);
+        rbTree.insert(25);
+        rbTree.insert(35);
+        rbTree.insert(5);
+        rbTree.insert(40);
+        rbTree.insert(15);
+        rbTree.insert(7);
+        rbTree.insert(7);
+
+        // Check if the root is black
+        expect(rbTree.root?.color).toBe("BLACK");
+
+        // Check if the tree is balanced
+        const [balanced] = isBalanced(rbTree.root);
+        const isRedBlack = hasRedParentBlackChildren(rbTree.root);
+        expect(balanced).toBe(true);
+        expect(isRedBlack).toBe(true);
     });
 
-    test("should maintain balance after multiple deletions", () => {
-        rbTree.insert(10);
-        rbTree.insert(20);
-        rbTree.insert(30);
-        rbTree.delete(20);
-        // Add more checks to verify the tree is balanced
-    });
+    // test("should maintain balance after multiple deletions", () => {
+    //     rbTree.insert(10);
+    //     rbTree.insert(20);
+    //     rbTree.insert(30);
+    //     rbTree.delete(20);
+    //     // Add more checks to verify the tree is balanced
+    // });
 });
+
+const isBalanced = (
+    node: RBNode | null,
+    blackCount: number = 0,
+): [boolean, number] => {
+    if (!node) {
+        return [true, blackCount + 1];
+    }
+
+    if (node.color === "BLACK") {
+        blackCount++;
+    }
+
+    if (node.color === "RED") {
+        if (node.left?.color === "RED" || node.right?.color === "RED") {
+            return [false, blackCount];
+        }
+    }
+
+    const [leftBalanced, leftBlackCount] = isBalanced(node.left, blackCount);
+    const [rightBalanced, rightBlackCount] = isBalanced(node.right, blackCount);
+
+    return [
+        leftBalanced && rightBalanced && leftBlackCount === rightBlackCount,
+        leftBlackCount,
+    ];
+};
+
+const hasRedParentBlackChildren = <T>(node: RBNode | null): boolean => {
+    if (!node) {
+        return true;
+    }
+
+    if (node.color === "RED") {
+        if (node.left?.color === "RED" || node.right?.color === "RED") {
+            return false;
+        }
+    }
+
+    return (
+        hasRedParentBlackChildren(node.left) &&
+        hasRedParentBlackChildren(node.right)
+    );
+};
