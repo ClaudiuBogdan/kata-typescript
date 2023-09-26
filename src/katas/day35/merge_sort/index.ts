f721d259eb5d211d8f175fb9f8f19337d0254775
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-09-26 10:08:33 +0300

    feat(day35): solve merge sort problem

--- src/katas/day35/merge_sort/index.ts
+++ src/katas/day35/merge_sort/index.ts
@@ -15,6 +15,58 @@
  * T(n) = 2T(n/2) + O(n)
  */
 
-export default function mergeSort(arr: number[]): number[] {
-  
+export default function sort(arr: number[]): number[] {
+    const sortedArr = new Array(arr.length);
+    mergeSort(arr, sortedArr, 0, arr.length - 1);
+    return sortedArr;
+}
+
+function mergeSort(
+    arr: number[],
+    sortedArr: number[],
+    lo: number,
+    hi: number,
+): void {
+    if (hi <= lo) {
+        return;
+    }
+    const middle = lo + Math.floor((hi - lo) / 2);
+    mergeSort(arr, sortedArr, lo, middle);
+    mergeSort(arr, sortedArr, middle + 1, hi);
+    merge(arr, sortedArr, lo, middle, hi);
+}
+
+function merge(
+    arr: number[],
+    sortedArr: number[],
+    lo: number,
+    middle: number,
+    hi: number,
+): void {
+    let left = lo;
+    let right = middle + 1;
+    let idx = lo;
+    while (left <= middle && right <= hi) {
+        if (arr[left] < arr[right]) {
+            sortedArr[idx] = arr[left];
+            left++;
+        } else {
+            sortedArr[idx] = arr[right];
+            right++;
+        }
+        idx++;
+    }
+    while (left <= middle) {
+        sortedArr[idx] = arr[left];
+        left++;
+        idx++;
+    }
+    while (right <= hi) {
+        sortedArr[idx] = arr[right];
+        right++;
+        idx++;
+    }
+    for (let i = lo; i <= hi; i++) {
+        arr[i] = sortedArr[i];
+    }
 }
