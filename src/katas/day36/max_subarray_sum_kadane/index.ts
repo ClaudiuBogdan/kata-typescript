db0975ab6b7c714c45b2a255c409e4c48e821283
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-10-02 19:41:36 +0300

    feat(day36): solve kadane algorithm

--- src/katas/day36/max_subarray_sum_kadane/index.ts
+++ src/katas/day36/max_subarray_sum_kadane/index.ts
@@ -9,5 +9,14 @@
  * maxSubarraySum(nums);  // returns 6
  */
 export function maxSubarraySum(nums: number[]): number {
-    // Function implementation here
+    if (nums.length === 0) {
+        return 0
+    }
+    let currentMax = nums[0];
+    let globalMax = currentMax;
+    for (let i = 1; i < nums.length; i++) {
+        currentMax = Math.max(nums[i], currentMax + nums[i]);
+        globalMax = Math.max(globalMax, currentMax);
+    }
+    return globalMax;
 }
