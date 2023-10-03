82a53c1eb583017953f63d01c140173401f8e552
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-10-03 11:54:52 +0300

    feat(day37): add medium difficulty algorithms

---
+++ src/katas/day37/avl_tree/meta.ts
@@ -0,0 +1,8 @@
+export const meta: TemplateMetaData = {
+    name: "AVL Tree",
+    id: "binary_tree/avl_tree",
+    category: "binary_tree",
+    difficulty: "medium",
+    disabled: false,
+    startTime: "2023-10-03T10:55:48+0300"
+};
