5bbe47323a68d4f2ca1e7a920e41d965193b7558
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-10-04 08:42:43 +0300

    remove(day37): remove medium problems

--- src/katas/day37/coin_change/meta.ts
+++
@@ -1,7 +0,0 @@
-export const meta: TemplateMetaData = {
-    name: "Coin Change",
-    id: "dynamic_programming/coin_change",
-    category: "dynamic_programming",
-    difficulty: "medium",
-    disabled: false,
-};
--- src/katas/day37/knapsack/meta.ts
+++
@@ -1,7 +0,0 @@
-export const meta: TemplateMetaData = {
-    name: "Knapsack",
-    id: "dynamic_programming/knapsack",
-    category: "dynamic programming",
-    difficulty: "medium",
-    disabled: false,
-};
