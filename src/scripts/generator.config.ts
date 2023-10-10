59e8e5638572e68c0aaf937bdf3e4b4ec0f57d56
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-10-10 10:56:07 +0300

    feat(generator): add medium and hard problems

--- src/scripts/generator.config.ts
+++ src/scripts/generator.config.ts
@@ -10,18 +10,18 @@ export const freqConfig: FreqConfig[] = [
         difficulty: "easy",
         count: 5,
     },
-    // {
-    //     difficulty: "medium",
-    //     count: 2,
-    // },
-    // {
-    //     difficulty: "hard",
-    //     count: 1,
-    // },
     {
-        ids: [
-            "binary_tree/avl_tree",
-            "binary_tree/red_black_tree"
-        ],
+        difficulty: "medium",
+        count: 2,
     },
+    {
+        difficulty: "hard",
+        count: 1,
+    },
+    // {
+    //     ids: [
+    //         "binary_tree/avl_tree",
+    //         "binary_tree/red_black_tree"
+    //     ],
+    // },
 ];
