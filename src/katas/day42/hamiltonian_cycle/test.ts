b571f314c431a4848658502337ae9338aa098a4e
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-10-10 10:57:11 +0300

    feat(day42): generate day 42

---
+++ src/katas/day42/hamiltonian_cycle/test.ts
@@ -0,0 +1,32 @@
+import { hamiltonianCycle } from "./index";
+
+describe("Hamiltonian Cycle Algorithm", () => {
+    test("should return null for an empty graph", () => {
+        expect(hamiltonianCycle([])).toBeNull();
+    });
+
+    test("should return a cycle for a graph with one vertex", () => {
+        expect(hamiltonianCycle([[0]])).toEqual([0, 0]);
+    });
+
+    test("should return a valid Hamiltonian cycle", () => {
+        const graph = [
+            [0, 1, 0, 1, 0],
+            [1, 0, 1, 1, 1],
+            [0, 1, 0, 0, 1],
+            [1, 1, 0, 0, 1],
+            [0, 1, 1, 1, 0],
+        ];
+        const result = hamiltonianCycle(graph);
+        expect(result).toEqual([0, 1, 2, 4, 3, 0]);
+    });
+
+    test("should return null for graphs without a Hamiltonian cycle", () => {
+        const graph = [
+            [0, 1, 0],
+            [1, 0, 0],
+            [0, 0, 0],
+        ];
+        expect(hamiltonianCycle(graph)).toBeNull();
+    });
+});
