777c282d1f2258dbabc750f3bfdc9e62542a31d7
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-10-11 16:34:13 +0300

    feat(day43): solve lcs w/ dp problem

--- src/katas/day43/longest_common_subsequence/index.ts
+++ src/katas/day43/longest_common_subsequence/index.ts
@@ -14,5 +14,35 @@ export default function longestCommonSubsequence(
     str1: string,
     str2: string,
 ): string {
-    // Function implementation here
+    const m = str1.length;
+    const n = str2.length;
+    const dp: number[][] = Array.from({ length: m + 1 }, () =>
+        new Array(n + 1).fill(0),
+    );
+
+    for (let i = 1; i <= str1.length; i++) {
+        for (let j = 1; j <= str2.length; j++) {
+            if (str1[i - 1] === str2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    let i = str1.length;
+    let j = str2.length;
+    let lcs = "";
+    while (i > 0 && j > 0) {
+        if (str1[i - 1] === str2[j - 1]) {
+            lcs = str1[i - 1] + lcs;
+            i--;
+            j--;
+        } else if (dp[i - 1][j] > dp[i][j - 1]) {
+            i--;
+        } else {
+            j--;
+        }
+    }
+    return lcs;
 }
