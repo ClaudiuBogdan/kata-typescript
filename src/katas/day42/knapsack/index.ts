dc0bed290feb3de8ff37e5215c21ecc80249b793
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-10-11 11:01:58 +0300

    feat(day42): solve knapsack problem

--- src/katas/day42/knapsack/index.ts
+++ src/katas/day42/knapsack/index.ts
@@ -1,5 +1,5 @@
 /**
- * Solves the 0/1 Knapsack Problem to find the maximum value that can be achieved within a given weight limit.
+ * Solves the `0/1 Knapsack Problem` to find the maximum value that can be achieved within a given weight limit.
  *
  * @param {number[]} weights - An array of integers representing the weights of the items.
  * @param {number[]} values - An array of integers representing the values of the items.
@@ -10,10 +10,26 @@
  * knapsack([1, 3, 4, 5], [1, 4, 5, 7], 7);  // returns 9 (items with weight 3 and 4, values 4 and 5)
  * knapsack([2, 3, 4, 5], [3, 4, 5, 6], 5);  // returns 7 (items with weight 2 and 3, values 3 and 4)
  */
-function knapsack(
+export default function knapsack(
     weights: number[],
     values: number[],
     capacity: number,
 ): number {
-    // Function implementation here
+    const dp: number[][] = Array.from({ length: weights.length + 1 }, () =>
+        new Array(capacity + 1).fill(0),
+    );
+
+    for (let i = 1; i <= weights.length; i++) {
+        for (let w = 1; w <= capacity; w++) {
+            if (weights[i - 1] <= w) {
+                dp[i][w] = Math.max(
+                    values[i - 1] + dp[i - 1][w - weights[i - 1]],
+                    dp[i - 1][w],
+                );
+            } else {
+                dp[i][w] = dp[i - 1][w];
+            }
+        }
+    }
+    return dp[weights.length][capacity];
 }
