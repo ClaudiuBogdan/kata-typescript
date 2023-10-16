41762dd975ccfb38370e1edd0ca192c81831ce23
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-10-16 12:18:11 +0300

    feat(day46): solve binary tree invert problem

--- src/katas/day46/binary_tree_invert/index.ts
+++ src/katas/day46/binary_tree_invert/index.ts
@@ -1,8 +1,16 @@
 /**
  * @function invertTree
  * @description To invert the tree, the left child should become the right child, and the right child should become the left.
- * @param root 
+ * @param root
  */
-export default function invertTree(root: BinaryNode<number> | null): BinaryNode<number> | null {
-
-};
+export default function invertTree(
+    root: BinaryNode<number> | null,
+): BinaryNode<number> | null {
+    if (root === null) {
+        return null;
+    }
+    const { left, right } = root;
+    root.left = invertTree(right);
+    root.right = invertTree(left);
+    return root;
+}
