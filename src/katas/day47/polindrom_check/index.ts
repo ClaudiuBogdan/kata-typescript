24dee5e6f7623a37071be96f25334bec2e5f83b2
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-10-20 11:33:51 +0300

    feat(day47): solve polindrom check problem

--- src/katas/day47/polindrom_check/index.ts
+++ src/katas/day47/polindrom_check/index.ts
@@ -9,5 +9,15 @@
  * isPalindrome("hello");    // returns false
  */
 export function isPalindrome(str: string): boolean {
-  // Function implementation here
+    const input = str
+        .split("")
+        .filter((char) => char.match(/[a-z]/i))
+        .map((char) => char.toLowerCase());
+
+    for (let i = 0; i < input.length / 2; i++) {
+        if (input[i] !== input[input.length - i - 1]) {
+            return false;
+        }
+    }
+    return true;
 }
