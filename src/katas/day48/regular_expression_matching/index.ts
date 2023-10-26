16e4e816e767c3fb04fb8a4159194e3961567fdc
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-10-26 10:20:01 +0300

    feat(day48): generate day 48

---
+++ src/katas/day48/regular_expression_matching/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Determines if a given text string matches a given regular expression pattern.
+ *
+ * @param {string} text - The text string to match against the pattern.
+ * @param {string} pattern - The regular expression pattern to match. Supports '.' to match any single character and '*' to match zero or more of the preceding element.
+ * @returns {boolean} True if the text matches the pattern, otherwise false.
+ *
+ * @example
+ * isMatch("aa", "a*");  // returns true
+ * isMatch("mississippi", "mis*is*p*.");  // returns false
+ */
+export function isMatch(text: string, pattern: string): boolean {
+    // Function implementation here
+}
