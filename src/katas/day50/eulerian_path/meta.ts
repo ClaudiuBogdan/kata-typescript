78b35d2741a1e174e4ec1d4429bbe75c47bdc034
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-10-30 21:55:44 +0200

    remove(day50): unsolved problems

--- src/katas/day50/eulerian_path/meta.ts
+++
@@ -1,7 +0,0 @@
-export const meta: TemplateMetaData = {
-    name: "Eulerian Path",
-    id: "graph/eulerian_path",
-    category: "graph",
-    difficulty: "medium",
-    disabled: false,
-};
--- src/katas/day50/floyd_warshall/meta.ts
+++
@@ -1,7 +0,0 @@
-export const meta: TemplateMetaData = {
-    name: "Floyd Warshall",
-    id: "graph/floyd_warshall",
-    category: "graph",
-    difficulty: "medium",
-    disabled: false,
-};
