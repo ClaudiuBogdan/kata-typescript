12b87341827e8e109720f91373012dc2100e7881
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-10-30 15:02:11 +0200

    feat(day50): solve binary tree in order traversal problem

--- src/katas/day50/binary_tree_in_order/index.ts
+++ src/katas/day50/binary_tree_in_order/index.ts
@@ -8,8 +8,18 @@
  */
 
 const in_order_traversal = (root: BinaryNode<number>): number[] => {
-
+    return traversal(root, []);
 };
 
+function traversal(root: BinaryNode<number> | null, path: number[]): number[] {
+    if (!root) {
+        return path;
+    }
+
+    traversal(root.left, path);
+    path.push(root.value);
+    traversal(root.right, path);
+    return path;
+}
 
 export default in_order_traversal;
