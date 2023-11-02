5f35f88bc7f0cbe9e6bd2d357904c22173e9652a
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-11-02 12:18:54 +0200

    feat(templates): update graph bridges template

--- src/templates/graph/graph_bridges/index.ts
+++ src/templates/graph/graph_bridges/index.ts
@@ -1,11 +1,4 @@
 /**
- * Represents an undirected graph using an adjacency list.
- */
-interface Graph {
-  [key: number]: number[];
-}
-
-/**
  * Represents a bridge in the graph as a tuple of vertices.
  */
 type Bridge = [number, number];
@@ -17,14 +10,14 @@ type Bridge = [number, number];
  * @returns {Bridge[]} - An array of bridges in the graph.
  *
  * @example
- * const graph = {
- *   0: [1, 2],
- *   1: [0, 2],
- *   2: [0, 1, 3],
- *   3: [2]
- * };
+ * const graph = [
+ *   [1, 2],
+ *   [0, 2],
+ *   [0, 1, 3],
+ *   [2]
+ * ];
  * findBridges(graph);  // returns [[2, 3]]
  */
-export function findBridges(graph: Graph): Bridge[] {
-  // Function implementation here
+export function findBridges(graph: AdjacencyList): Bridge[] {
+    // Function implementation here
 }
