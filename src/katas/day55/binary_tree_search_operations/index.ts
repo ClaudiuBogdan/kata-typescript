a6607bd2d0fc929842f5bed92d67cbc5934b9731
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-11-06 13:14:12 +0200

    feat(day55): solve binary tree search op problem

--- src/katas/day55/binary_tree_search_operations/index.ts
+++ src/katas/day55/binary_tree_search_operations/index.ts
@@ -5,6 +5,7 @@ export class TreeNode {
     val: number;
     left: TreeNode | null;
     right: TreeNode | null;
+
     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
         this.val = val === undefined ? 0 : val;
         this.left = left === undefined ? null : left;
@@ -27,7 +28,31 @@ export class BST {
      * @param {number} val - The value to insert.
      */
     insert(val: number): void {
-        // Function implementation here
+        let parent = this.root;
+        let node = parent;
+        while (node) {
+            parent = node;
+            if (val === node.val) {
+                return;
+            }
+            if (val < node.val) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+        }
+        const newNode: TreeNode = {
+            val,
+            left: null,
+            right: null,
+        };
+        if (!parent) {
+            this.root = newNode;
+        } else if (val < parent.val) {
+            parent.left = newNode;
+        } else {
+            parent.right = newNode;
+        }
     }
 
     /**
@@ -36,7 +61,18 @@ export class BST {
      * @returns {boolean} True if the value is found, otherwise false.
      */
     search(val: number): boolean {
-        // Function implementation here
+        let node = this.root;
+
+        while (node) {
+            if (node.val === val) {
+                return true;
+            } else if (val < node.val) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+        }
+        return false;
     }
 
     /**
@@ -44,6 +80,46 @@ export class BST {
      * @param {number} val - The value to delete.
      */
     delete(val: number): void {
-        // Function implementation here
+        // TODO: iterative approach https://chat.openai.com/c/ee8e6bfc-e5a2-478e-8080-142852cfee9d
+        let parent: TreeNode | null = null;
+        let node: TreeNode | null = this.root;
+        while (node && node.val !== val) {
+            parent = node;
+            if (val < node.val) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+        }
+
+        if (!node) {
+            return;
+        }
+
+        // If both left and right are not null
+        if (node.left && node.right) {
+            let successor = node.right;
+            let successorParent = node;
+
+            while (successor.left !== null) {
+                successorParent = successor;
+                successor = successor.left;
+            }
+            node.val = successor.val;
+            node = successor;
+            parent = successorParent;
+        }
+
+        const child: TreeNode | null = node.left ? node.left : node.right;
+
+        if (parent === null) {
+            this.root = child;
+        } else {
+            if (node === parent.left) {
+                parent.left = child;
+            } else {
+                parent.right = child;
+            }
+        }
     }
 }
