b3d105b128538a117af76244e54afb9383c9895e
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-11-06 18:46:59 +0200

    remove(day55): unsolved problems

--- src/katas/day55/maximum_bipartite_matching/meta.ts
+++
@@ -1,7 +0,0 @@
-export const meta: TemplateMetaData = {
-    name: "Maximum Bipartite Matching",
-    id: "graph/maximum_bipartite_matching",
-    category: "graph",
-    difficulty: "medium",
-    disabled: false,
-};
--- src/katas/day55/maximum_flow/meta.ts
+++
@@ -1,7 +0,0 @@
-export const meta: TemplateMetaData = {
-    name: "Maximum Flow",
-    id: "graph/maximum_flow",
-    category: "graph",
-    difficulty: "medium",
-    disabled: false,
-};
