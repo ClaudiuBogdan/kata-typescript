766e08006de2d1b837b341f0824e52887addea67
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-11-09 14:27:59 +0200

    feat(day58): solve graph list bfs problem

--- src/katas/day58/graph_list_bfs/index.ts
+++ src/katas/day58/graph_list_bfs/index.ts
@@ -1,10 +1,45 @@
 /**
- * @function dfs - Breadth first search
+ * @function bfs - Breadth first search
  * @description Searches for a path from source to target in a graph. Breadth first search means that we search the graph layer by layer, starting from the source adding the next layer of nodes to the queue.
- * @param graph 
- * @param source 
- * @param target 
+ * @param graph
+ * @param source
+ * @param target
  */
-export default function bfs(graph: WeightedAdjacencyList, source: number, target: number): number[] | null {
-  
+export default function bfs(
+    graph: WeightedAdjacencyList,
+    source: number,
+    target: number,
+): number[] | null {
+    const seen: boolean[] = new Array(graph.length).fill(false);
+    const prev: number[] = new Array(graph.length).fill(-1);
+    const queue: number[] = [];
+    queue.push(source);
+
+    while (queue.length > 0) {
+        const node = queue.shift()!;
+        seen[node] = true;
+        if (node === target) {
+            return getPath(prev, target);
+        }
+
+        for (const edge of graph[node]) {
+            if (seen[edge.to]) {
+                continue;
+            }
+            prev[edge.to] = node;
+            queue.push(edge.to);
+        }
+    }
+
+    return null;
+}
+
+function getPath(prev: number[], target: number): number[] {
+    const path: number[] = [];
+    let node = target;
+    while (node !== -1) {
+        path.push(node);
+        node = prev[node];
+    }
+    return path.reverse();
 }
