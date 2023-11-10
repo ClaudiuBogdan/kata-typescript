4726a5987bb708d54248f8daeabbb1514376f01f
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-11-10 15:29:19 +0200

    feat(day59): solve graph list dfs problem

--- src/katas/day59/graph_list_dfs/index.ts
+++ src/katas/day59/graph_list_dfs/index.ts
@@ -1,11 +1,44 @@
 /**
  * @function DepthFirstSearch - Depth first search on graph
- * @description 
+ * @description
  * @link https://www.geeksforgeeks.org/depth-first-search-or-dfs-for-a-graph/
  * @param graph WeightedAdjacencyList
- * @param source 
- * @param needle 
+ * @param source
+ * @param needle
  */
-export default function dfs(graph: WeightedAdjacencyList, source: number, target: number): number[] | null {
+export default function dfs(
+    graph: WeightedAdjacencyList,
+    source: number,
+    target: number,
+): number[] | null {
+    const seen: boolean[] = new Array(graph.length).fill(false);
+    const prev: number[] = new Array(graph.length).fill(-1);
+    const stack: number[] = [];
+    stack.push(source);
 
+    while (stack.length > 0) {
+        const node = stack.pop()!;
+        seen[node] = true;
+        if (node === target) {
+            return getPath(prev, target);
+        }
+        for (const edge of graph[node]) {
+            if (!seen[edge.to]) {
+                prev[edge.to] = node;
+                stack.push(edge.to);
+            }
+        }
+    }
+
+    return null;
+}
+
+function getPath(prev: number[], target: number): number[] {
+    const path: number[] = [];
+    let node = target;
+    while (node !== -1) {
+        path.push(node);
+        node = prev[node];
+    }
+    return path.reverse();
 }
