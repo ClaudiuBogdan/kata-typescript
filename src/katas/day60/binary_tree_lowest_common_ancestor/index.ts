1a6909f4e35525fc8b0a515e06f15d5e8d57b45e
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-11-11 09:38:02 +0200

    feat(day60): solve bt lowest common ancestor problem

--- src/katas/day60/binary_tree_lowest_common_ancestor/index.ts
+++ src/katas/day60/binary_tree_lowest_common_ancestor/index.ts
@@ -1,4 +1,31 @@
 /**
+ * Finds the lowest common ancestor of two nodes in a binary tree.
+ *
+ * @param {TreeNode | null} root - The root node of the binary tree.
+ * @param {TreeNode} p - The first node.
+ * @param {TreeNode} q - The second node.
+ * @returns {TreeNode | null} The lowest common ancestor node, or null if not found.
+ */
+export function lowestCommonAncestor(
+    root: TreeNode | null,
+    p: TreeNode,
+    q: TreeNode,
+): TreeNode | null {
+    if (root === null || root.val === p.val || root.val === q.val) {
+        return root;
+    }
+
+    const left = lowestCommonAncestor(root.left, p, q);
+    const right = lowestCommonAncestor(root.right, p, q);
+
+    if (left !== null && right !== null) {
+        return root;
+    }
+
+    return left === null ? right : left;
+}
+
+/**
  * Definition for a binary tree node.
  */
 export class TreeNode {
@@ -10,26 +37,4 @@ export class TreeNode {
         this.left = left === undefined ? null : left;
         this.right = right === undefined ? null : right;
     }
-}
-
-/**
- * Finds the lowest common ancestor of two nodes in a binary tree.
- *
- * @param {TreeNode | null} root - The root node of the binary tree.
- * @param {TreeNode} p - The first node.
- * @param {TreeNode} q - The second node.
- * @returns {TreeNode | null} The lowest common ancestor node, or null if not found.
- *
- * @example
- * const root = new TreeNode(3, new TreeNode(5), new TreeNode(1));
- * const p = root.left;
- * const q = root.right;
- * lowestCommonAncestor(root, p, q);  // returns root
- */
-export function lowestCommonAncestor(
-    root: TreeNode | null,
-    p: TreeNode,
-    q: TreeNode,
-): TreeNode | null {
-    // Function implementation here
 }
