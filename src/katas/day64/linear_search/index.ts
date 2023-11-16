2dbff2c51bd837b13668a3159160d7e316640556
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-11-16 11:32:10 +0200

    feat(day64): solve linear search problem

--- src/katas/day64/linear_search/index.ts
+++ src/katas/day64/linear_search/index.ts
@@ -12,5 +12,11 @@
  * @example linearSearch([1,5,6], 2) => -1
  */
 export default function linearSearch(arr: number[], target: number): number {
-
+    for (let i = 0; i < arr.length; i++) {
+        const val = arr[i];
+        if (val === target) {
+            return i;
+        }
+    }
+    return -1;
 }
