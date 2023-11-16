987d6aabed38b3602391c20b2cc4618c20c4a427
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-11-16 12:26:41 +0200

    feat(day64): solve topological sort problem

--- src/katas/day64/topological_sort/index.ts
+++ src/katas/day64/topological_sort/index.ts
@@ -16,5 +16,38 @@
  * topologicalSort(graph);  // returns [0, 2, 1, 3, 4] or null if a cycle is detected
  */
 export function topologicalSort(graph: AdjacencyList): number[] | null {
-    // Function implementation here
+    const stack: number[] = [];
+    const visited: boolean[] = new Array(graph.length).fill(false);
+    const recStack: boolean[] = new Array(graph.length).fill(false);
+
+    const dfs = (node: number): boolean => {
+        if (recStack[node]) {
+            return true;
+        }
+
+        if (visited[node]) {
+            return false;
+        }
+
+        visited[node] = true;
+        recStack[node] = true;
+
+        for (const neighbor of graph[node]) {
+            if (dfs(neighbor)) {
+                return true;
+            }
+        }
+
+        stack.push(node);
+        recStack[node] = false;
+        return false;
+    };
+
+    for (let i = 0; i < graph.length; i++) {
+        if (dfs(i)) {
+            return null;
+        }
+    }
+
+    return stack.reverse();
 }
