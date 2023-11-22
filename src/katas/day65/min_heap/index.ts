e2c78919252f882140f6111614db26fbe232174c
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-11-22 10:34:25 +0200

    feat(day65): solve min heap problem

--- src/katas/day65/min_heap/index.ts
+++ src/katas/day65/min_heap/index.ts
@@ -6,14 +6,71 @@ import { IHeap } from "./types";
  * @link - https://www.geeksforgeeks.org/heap-data-structure/
  */
 export default class MinHeap<T> implements IHeap<T> {
-    readonly size: number;
+    private heap: T[];
+
+    public get size(): number {
+        return this.heap.length;
+    }
+
+    constructor() {
+        this.heap = [];
+    }
+
     insert(value: T): void {
-        throw new Error("Method not implemented.");
+        this.heap.push(value);
+        this.bubbleUp(this.size - 1);
     }
+
     peek(): T | undefined {
-        throw new Error("Method not implemented.");
+        if (this.size === 0) {
+            return;
+        }
+        return this.heap[0];
     }
+
     pop(): T | undefined {
-        throw new Error("Method not implemented.");
+        if (this.size === 0) {
+            return;
+        }
+        if (this.size === 1) {
+            return this.heap.pop();
+        }
+        const minVal = this.heap[0];
+        this.heap[0] = this.heap.pop()!;
+        this.bubbleDown(0);
+
+        return minVal;
+    }
+
+    private bubbleUp(idx: number): void {
+        if (idx <= 0) {
+            return;
+        }
+        const heap = this.heap;
+        const parentIdx = Math.floor((idx - 1) / 2);
+        if (heap[idx] < heap[parentIdx]) {
+            [heap[idx], heap[parentIdx]] = [heap[parentIdx], heap[idx]];
+            return this.bubbleUp(parentIdx);
+        }
+    }
+
+    private bubbleDown(idx: number): void {
+        if (idx >= this.size - 1) {
+            return;
+        }
+        const leftIdx = idx * 2 + 1;
+        const rightIdx = idx * 2 + 2;
+        if (leftIdx >= this.size) {
+            return;
+        }
+        const heap = this.heap;
+        const minIdx =
+            rightIdx < this.size && heap[rightIdx] < heap[leftIdx]
+                ? rightIdx
+                : leftIdx;
+        if (heap[minIdx] < heap[idx]) {
+            [heap[idx], heap[minIdx]] = [heap[minIdx], heap[idx]];
+            this.bubbleDown(minIdx);
+        }
     }
 }
