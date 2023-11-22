7e7a16d6fc44e32da3b1577686eb19b6311e0bbb
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-11-22 09:48:43 +0200

    feat(day65): solve activity selection problem

--- src/katas/day65/activity_selection/index.ts
+++ src/katas/day65/activity_selection/index.ts
@@ -5,7 +5,19 @@
  * @returns {Activity[]} An array of selected activities that don't overlap.
  */
 export function activitySelection(activities: Activity[]): Activity[] {
-    // Function implementation here
+    if (activities.length === 0) {
+        return [];
+    }
+    const results: Activity[] = [];
+    const sorted = activities.sort((a, b) => a.end - b.end);
+    let lastEnd = 0;
+    for (const activity of sorted) {
+        if (activity.start >= lastEnd) {
+            results.push(activity);
+            lastEnd = activity.end;
+        }
+    }
+    return results;
 }
 
 /**
