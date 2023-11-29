b1335b20d338151e4abc21cedbf316e5c91cda24
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-11-29 12:26:46 +0200

    feat(day67): generate day 67

---
+++ src/katas/day67/binary_tree_in_order/index.ts
@@ -0,0 +1,25 @@
+/**
+ * @function BinaryTreeInOrderTraversal
+ * @param root {BinaryNode<number>}
+ * @returns {number[]}
+ * @description Given the root of a binary tree, return the inorder traversal of its nodes' values. The root node is visited between the left and right subtrees.
+ * @see https://www.geeksforgeeks.org/inorder-traversal-of-binary-tree/
+ *
+ */
+
+const in_order_traversal = (root: BinaryNode<number>): number[] => {
+  return traversal(root, [])
+};
+
+const traversal = (root: BinaryNode<number> | null, path: number[]): number[] => {
+  if(!root){
+    return path;
+  }
+  traversal(root.left, path);
+  path.push(root.value);
+  traversal(root.right, path);
+  return path;
+}
+
+
+export default in_order_traversal;
---
+++ src/katas/day67/quick_sort/meta.ts
@@ -0,0 +1,7 @@
+export const meta: TemplateMetaData = {
+    name: "Quick Sort",
+    id: "sort/quick_sort",
+    category: "sort",
+    difficulty: "easy",
+    disabled: false,
+};
