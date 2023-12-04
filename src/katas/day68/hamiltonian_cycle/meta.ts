2a7eaf4be9cee233247e98af6afd9f0e5142cb43
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-12-04 16:38:25 +0200

    remove(day68): unsolved problems

--- src/katas/day68/hamiltonian_cycle/meta.ts
+++
@@ -1,7 +0,0 @@
-export const meta: TemplateMetaData = {
-    name: "Hamiltonian Cycle",
-    id: "graph/hamiltonian_cycle",
-    category: "graph",
-    difficulty: "hard",
-    disabled: false,
-};
