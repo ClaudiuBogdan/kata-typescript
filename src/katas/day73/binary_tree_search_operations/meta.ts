0dfd9d07fc3d105223fd65ecfc2b270fee8fd99e
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-12-14 10:35:44 +0200

    feat(73): generate day 73

---
+++ src/katas/day73/binary_tree_search_operations/meta.ts
@@ -0,0 +1,7 @@
+export const meta: TemplateMetaData = {
+    name: "Binary Tree Search Operations",
+    id: "binary_tree/binary_tree_search_operations",
+    category: "binary_tree",
+    difficulty: "easy",
+    disabled: false,
+};
