bf0f82cb4ab51f778481dea593000be5b512ae32
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-12-18 10:50:28 +0200

    feat(76): generate day 76

---
+++ src/katas/day76/polindrom_check/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Checks if a given string is a palindrome.
+ *
+ * @param {string} str - The string to check.
+ * @returns {boolean} True if the string is a palindrome, otherwise false.
+ *
+ * @example
+ * isPalindrome("racecar");  // returns true
+ * isPalindrome("hello");    // returns false
+ */
+export function isPalindrome(str: string): boolean {
+  // Function implementation here
+}
