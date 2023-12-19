2dd86061e9d8449b09a54c4daa2c4a577be646de
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-12-19 17:34:04 +0200

    feat(day77): solve graph list dfs problem

--- src/katas/day77/graph_list_dfs/index.ts
+++ src/katas/day77/graph_list_dfs/index.ts
@@ -6,6 +6,41 @@
  * @param source 
  * @param needle 
  */
-export default function dfs(graph: WeightedAdjacencyList, source: number, target: number): number[] | null {
+export default function dfs(
+    graph: WeightedAdjacencyList,
+    source: number,
+    target: number,
+): number[] | null {
+    const seen: boolean[] = new Array(graph.length).fill(false);
+    const prev: number[] = new Array(graph.length).fill(-1);
 
+    const search = (node: number): number[] | null => {
+        seen[node] = true;
+        if (node === target) {
+            return getPath(prev, target);
+        }
+        for (let edge of graph[node]) {
+            if (seen[edge.to]) {
+                continue;
+            }
+            prev[edge.to] = node;
+            const path = search(edge.to);
+            if (path) {
+                return path;
+            }
+        }
+        return null;
+    };
+
+    return search(source);
+}
+
+function getPath(prev: number[], target: number): number[] {
+    const path: number[] = [];
+    let node = target;
+    while (node !== -1) {
+        path.push(node);
+        node = prev[node];
+    }
+    return path.reverse();
 }
