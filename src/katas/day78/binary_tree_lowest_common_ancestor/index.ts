8ed6977178344ee1e28fc5cc60b637115d083c12
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-12-20 19:13:17 +0200

    feat(78): solve bt lowest common acestor problem

--- src/katas/day78/binary_tree_lowest_common_ancestor/index.ts
+++ src/katas/day78/binary_tree_lowest_common_ancestor/index.ts
@@ -31,5 +31,16 @@ export function lowestCommonAncestor(
     p: TreeNode,
     q: TreeNode,
 ): TreeNode | null {
-    // Function implementation here
+    if (!root) {
+        return null;
+    }
+    if (root === p || root === q) {
+        return root;
+    }
+    const leftAncestor = lowestCommonAncestor(root.left, p, q);
+    const rightAncestor = lowestCommonAncestor(root.right, p, q);
+    if (leftAncestor && rightAncestor) {
+        return root;
+    }
+    return leftAncestor || rightAncestor; 
 }
