36ed4680584f412b9b86518743a6ae8726092663
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-12-23 09:45:55 +0200

    feat(80): generate day 80

---
+++ src/katas/day80/merge_sort/index.ts
@@ -0,0 +1,20 @@
+/**
+ * @function mergeSort
+ * @description keeps on dividing the list into equal halves until it can no more be divided. By definition, if it is only one element in the list, it is sorted.
+ * @link https://github.com/TheAlgorithms/TypeScript/blob/2603b2245ce073e18f096d3484335eb12b9d072c/sorts/merge_sort.ts#L1
+ * @see [Merge Sort](https://www.javatpoint.com/merge-sort)
+ * @example MergeSort([8, 3, 5, 1, 4, 2]) = [1, 2, 3, 4, 5, 8]
+ * @Complexity_Analysis
+ * Space complexity - O(n)
+ * Time complexity
+ *      Best case   -   O(nlogn)
+ *      Worst case  -   O(nlogn)
+ *      Average case -  O(nlogn)
+ *
+ * Merge Sort is a recursive algorithm and time complexity can be expressed as following recurrence relation.
+ * T(n) = 2T(n/2) + O(n)
+ */
+
+export default function mergeSort(arr: number[]): number[] {
+  
+}
