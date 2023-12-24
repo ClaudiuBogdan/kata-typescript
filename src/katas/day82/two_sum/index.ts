58b21e67f28332a517a28ac0736a38195c2c858c
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-12-24 13:57:01 +0200

    feat(82): solve two sum problem

--- src/katas/day82/two_sum/index.ts
+++ src/katas/day82/two_sum/index.ts
@@ -11,5 +11,16 @@
  * twoSum(nums, target);  // returns [0, 1]
  */
 export function twoSum(nums: number[], target: number): number[] | null {
-    // Function implementation here
+    const compMap = new Map<number, number>();
+    for (let i = 0; i < nums.length; i++) {
+        compMap.set(nums[i], i);
+    }
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        const idx = compMap.get(complement);
+        if (idx !== undefined && i !== idx) {
+            return [i, idx];
+        }
+    }
+    return null;
 }
