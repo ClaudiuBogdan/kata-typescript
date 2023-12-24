a05b71b790a7488e1ffb938755528c8c54961ea1
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-12-24 10:08:42 +0200

    feat(82): solve dijkstra problem

--- src/katas/day82/dijkstra_list/index.ts
+++ src/katas/day82/dijkstra_list/index.ts
@@ -4,8 +4,61 @@
  * @link https://www.programiz.com/dsa/dijkstra-algorithm
  * @param target 
  * @param sink
- * @param arr 
+ * @param graph 
  */
-export default function dijkstra_list(source: number, target: number, arr: WeightedAdjacencyList): number[] {
+export default function dijkstra_list(
+    source: number,
+    target: number,
+    graph: WeightedAdjacencyList,
+): number[] {
+    const seen: boolean[] = new Array(graph.length).fill(false);
+    const dist: number[] = new Array(graph.length).fill(Infinity);
+    const prev: number[] = new Array(graph.length).fill(-1);
 
+    dist[source] = 0;
+
+    while (hasAvailableNode(seen, dist)) {
+        const node = getMinNode(seen, dist);
+        seen[node] = true;
+        if (node === target) {
+            return getPath(prev, target);
+        }
+        for (let edge of graph[node]) {
+            if (seen[edge.to]) {
+                continue;
+            }
+            const distance = dist[node] + edge.weight;
+            if (distance < dist[edge.to]) {
+                dist[edge.to] = distance;
+                prev[edge.to] = node;
+            }
+        }
+    }
+    return [];
+}
+
+function hasAvailableNode(seen: boolean[], dist: number[]): boolean {
+    return dist.some((distance, node) => !seen[node] && distance < Infinity);
+}
+
+function getMinNode(seen: boolean[], dist: number[]): number {
+    let minNode = -1;
+    let minDist = Infinity;
+    for (let i = 0; i < dist.length; i++) {
+        if (!seen[i] && dist[i] < minDist) {
+            minNode = i;
+            minDist = dist[i];
+        }
+    }
+    return minNode;
+}
+
+function getPath(prev: number[], target: number): number[] {
+    const path: number[] = [];
+    let node = target;
+    while (node !== -1) {
+        path.push(node);
+        node = prev[node];
+    }
+    return path.reverse();
 }
