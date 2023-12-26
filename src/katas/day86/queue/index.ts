54fdb69cd3d343418df55048150d40f2882916ce
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-12-26 11:19:50 +0200

    feat(86): solve queue problem

--- src/katas/day86/queue/index.ts
+++ src/katas/day86/queue/index.ts
@@ -1,18 +1,62 @@
 export default class Queue<T> {
-    public length: number;
+    private head: Node<T> | null;
+    private tail: Node<T> | null;
+    private len: number;
 
-    
+    public get length(): number {
+        return this.len;
+    }
 
     constructor() {
+        this.head = null;
+        this.tail = null;
+        this.len = 0;
     }
 
     enqueue(item: T): void {
+        const node: Node<T> = {
+            value: item,
+            next: null,
+            prev: this.tail,
+        };
+        if (this.len === 0) {
+            this.head = node;
+            this.tail = node;
+        } else {
+            this.tail!.next = node;
+            this.tail = node; // TODO: update the tail pointer
+        }
+        this.len++;
+    }
 
-}
     deque(): T | undefined {
+        if (this.len === 0) {
+            return undefined;
+        }
+        const oldHead = this.head!;
+        this.head = oldHead.next;
+        if (this.len === 1) {
+            this.tail = null;
+            this.head = null;
+        } else {
+            oldHead.next = null;
+            this.head!.prev = null;
+        }
+        this.len--;
+        return oldHead.value;
+    }
 
-}
     peek(): T | undefined {
+        if (this.len === 0) {
+            return undefined;
+        } else {
+            return this.head!.value;
+        }
+    }
+}
 
-}
-}
+type Node<T> = {
+    value: T;
+    next: Node<T> | null;
+    prev: Node<T> | null;
+};
