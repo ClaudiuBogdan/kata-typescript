2939f8e0bc0c54567b47ebc9e14b9dbb7cd3f834
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-12-26 10:44:44 +0200

    feat(86): generate day 86

---
+++ src/katas/day86/minimum_cut/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Finds the minimum cut of a weighted, undirected graph.
+ *
+ * @param {WeightedAdjacencyList} graph - The adjacency list representation of the graph.
+ * @returns {number}  the minimum cut value.
+ */
+export function findMinimumCut(graph: WeightedAdjacencyList): number {
+    // Function implementation here
+}
