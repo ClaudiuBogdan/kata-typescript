e492eacae65cdcdcf29e502e91ead9bf3b0b07ec
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2024-01-08 18:07:48 +0200

    feat(89): solve prims list problem

--- src/katas/day89/prims_list/index.ts
+++ src/katas/day89/prims_list/index.ts
@@ -3,8 +3,54 @@
  * @description Prim's algorithm is a minimum spanning tree algorithm that takes a graph as input and finds the subset of the edges of that graph which form a tree that includes every vertex, and has the minimum sum of weights among all the trees that can be formed from the graph
  * @link https://www.programiz.com/dsa/prim-algorithm
  * @link https://www.geeksforgeeks.org/prims-minimum-spanning-tree-mst-greedy-algo-5/
- * @param list 
+ * @param graph 
  */
-export default function prims(list: WeightedAdjacencyList): WeightedAdjacencyList | null {
+export default function prims(
+    graph: WeightedAdjacencyList,
+): WeightedAdjacencyList | null {
+    const seen: boolean[] = new Array(graph.length).fill(false);
+    const dist: number[] = new Array(graph.length).fill(Infinity);
+    const prev: number[] = new Array(graph.length).fill(-1);
+    const mst: WeightedAdjacencyList = Array.from(
+        { length: graph.length },
+        () => [],
+    );
+    dist[0] = 0;
 
+    while (hasAvailableNode(seen, dist)) {
+        const node = getMinNode(seen, dist);
+        seen[node] = true;
+        if (prev[node] !== -1) {
+            const parent = prev[node];
+            mst[parent].push({ to: node, weight: dist[node] });
+            mst[node].push({ to: parent, weight: dist[node] });
+        }
+        for (const edge of graph[node]) {
+            if (seen[edge.to]) {
+                continue;
+            }
+            const distance = edge.weight;
+            if (distance < dist[edge.to]) {
+                prev[edge.to] = node;
+                dist[edge.to] = distance;
+            }
+        }
+    }
+    return mst;
+}
+
+function hasAvailableNode(seen: boolean[], dist: number[]): boolean {
+    return dist.some((distance, node) => !seen[node] && dist[node] < Infinity);
+}
+
+function getMinNode(seen: boolean[], dist: number[]): number {
+    let minNode = -1;
+    let minDist = Infinity;
+    for (let v = 0; v < dist.length; v++) {
+        if (!seen[v] && dist[v] < minDist) {
+            minNode = v;
+            minDist = dist[v];
+        }
+    }
+    return minNode;
 }
