0bec2ec19913d0e209f905b2c5c8f7f12ce37b43
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2024-01-09 11:31:03 +0200

    feat(91): solve bt search op problem

--- src/katas/day91/binary_tree_search_operations/index.ts
+++ src/katas/day91/binary_tree_search_operations/index.ts
@@ -27,7 +27,32 @@ export class BST {
      * @param {number} val - The value to insert.
      */
     insert(val: number): void {
-        // Function implementation here
+        let node: TreeNode | null = this.root;
+        let parent: TreeNode | null = null;
+
+        while (node) {
+            parent = node;
+            if (val === node.val) {
+                return;
+            } else if (val < node.val) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+        }
+        const newNode: TreeNode = {
+            val,
+            left: null,
+            right: null,
+        };
+
+        if (parent === null) {
+            this.root = newNode;
+        } else if (val < parent.val) {
+            parent.left = newNode;
+        } else {
+            parent.right = newNode;
+        }
     }
 
     /**
@@ -36,7 +61,11 @@ export class BST {
      * @returns {boolean} True if the value is found, otherwise false.
      */
     search(val: number): boolean {
-        // Function implementation here
+        let node = this.root;
+        while (node && node.val !== val) {
+            node = val < node.val ? node.left : node.right;
+        }
+        return !!node;
     }
 
     /**
@@ -44,6 +73,35 @@ export class BST {
      * @param {number} val - The value to delete.
      */
     delete(val: number): void {
-        // Function implementation here
+        let node: TreeNode | null = this.root;
+        let parent: TreeNode | null = null;
+
+        while (node && node.val !== val) {
+            parent = node;
+            node = val < node.val ? node.left : node.right;
+        }
+        if (!node) {
+            return;
+        }
+        if (node.left && node.right) {
+            let successor = node.right;
+            let successorParent = node;
+            while (successor.left) {
+                successorParent = successor;
+                successor = successor.left;
+            }
+            node.val = successor.val;
+            node = successor;
+            parent = successorParent;
+        }
+        const child = node.left ? node.left : node.right;
+
+        if (parent === null) {
+            this.root = null;
+        } else if (node === parent.left) {
+            parent.left = child;
+        } else {
+            parent.right = child;
+        }
     }
 }
