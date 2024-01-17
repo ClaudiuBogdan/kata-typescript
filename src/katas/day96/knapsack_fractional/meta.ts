ffe3e64616797d6b03d225227e4b4be2f1b3bb11
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2024-01-17 12:14:23 +0200

    feat(96): remove unsolved problems

--- src/katas/day96/knapsack_fractional/meta.ts
+++
@@ -1,7 +0,0 @@
-export const meta: TemplateMetaData = {
-    name: "Knapsack Fractional",
-    id: "greedy/knapsack_fractional",
-    category: "greedy",
-    difficulty: "medium",
-    disabled: false,
-};
