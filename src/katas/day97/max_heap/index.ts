87936db9df5bf6097c13a4e39650bb6953ccc71a
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2024-01-17 12:40:57 +0200

    feat(97): solve max heap problem

--- src/katas/day97/max_heap/index.ts
+++ src/katas/day97/max_heap/index.ts
@@ -6,14 +6,71 @@ import { IHeap } from "./types";
  * @link - https://www.geeksforgeeks.org/heap-data-structure/
  */
 export default class MaxHeap<T> implements IHeap<T> {
-    readonly size: number;
+    private heap: T[];
+    public get size(): number {
+        return this.heap.length;
+    }
+
+    constructor() {
+        this.heap = [];
+    }
+
     insert(value: T): void {
-        throw new Error("Method not implemented.");
+        this.heap.push(value);
+        this.bubbleUp(this.size - 1);
     }
+
     peek(): T | undefined {
-        throw new Error("Method not implemented.");
+        if (this.size === 0) {
+            return;
+        }
+        return this.heap[0];
     }
+
     pop(): T | undefined {
-        throw new Error("Method not implemented.");
+        if (this.size === 0) {
+            return;
+        }
+        if (this.size === 1) {
+            return this.heap.pop();
+        }
+
+        const maxVal = this.heap[0];
+        this.heap[0] = this.heap.pop()!;
+        this.bubbleDown(0);
+        return maxVal;
+    }
+
+    private bubbleUp(idx: number): void {
+        if (idx <= 0) {
+            return;
+        }
+        const parentIdx = Math.floor((idx - 1) / 2);
+        const heap = this.heap;
+        if (heap[idx] > heap[parentIdx]) {
+            [heap[idx], heap[parentIdx]] = [heap[parentIdx], heap[idx]];
+            this.bubbleUp(parentIdx);
+        }
+    }
+
+    private bubbleDown(idx: number): void {
+        if (idx >= this.size - 1) {
+            return;
+        }
+        const leftIdx = idx * 2 + 1;
+        const rightIdx = idx * 2 + 2;
+        const heap = this.heap;
+
+        if (leftIdx >= this.size) {
+            return;
+        }
+        const maxIdx =
+            rightIdx < this.size && heap[rightIdx] > heap[leftIdx]
+                ? rightIdx
+                : leftIdx;
+        if (heap[idx] < heap[maxIdx]) {
+            [heap[idx], heap[maxIdx]] = [heap[maxIdx], heap[idx]];
+            this.bubbleDown(maxIdx);
+        }
     }
 }
