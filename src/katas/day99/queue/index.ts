142bb312eb7c7ea21db3682ba2532c7091aad2e1
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2024-01-23 19:49:31 +0200

    feat(99): solve queue problem

--- src/katas/day99/queue/index.ts
+++ src/katas/day99/queue/index.ts
@@ -1,18 +1,62 @@
 export default class Queue<T> {
-    public length: number;
+    private head: Node<T> | null;
+    private tail: Node<T> | null;
+    private len: number;
 
-    
+    public get length(): number {
+        return this.len;
+    }
 
     constructor() {
+        this.head = null;
+        this.tail = null;
+        this.len = 0;
     }
 
     enqueue(item: T): void {
+        const node: Node<T> = {
+            value: item,
+            prev: this.tail,
+            next: null,
+        };
+        if (this.len == 0) {
+            this.head = node;
+            this.tail = node;
+        } else {
+            this.tail!.next = node;
+            this.tail = node;
+        }
+        this.len++;
+    }
 
-}
     deque(): T | undefined {
-
-}
+        if (this.len === 0) {
+            return;
+        } else if (this.len === 1) {
+            const { value } = this.head!;
+            this.head = null;
+            this.tail = null;
+            this.len--;
+            return value;
+        } else {
+            const { value } = this.head!;
+            this.head = this.head!.next!;
+            this.head.prev = null;
+            this.len--;
+            return value;
+        }
+    }
     peek(): T | undefined {
+        if (this.len === 0) {
+            return;
+        }
 
+        return this.head!.value;
+    }
 }
-}
+
+type Node<T> = {
+    value: T;
+    prev: Node<T> | null;
+    next: Node<T> | null;
+};
