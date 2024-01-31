f602e132c3e8d8de1d140f5efa15fcccd01524d2
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2024-01-31 10:32:33 +0200

    feat(102): generate day 102

---
+++ src/katas/day102/max_subarray_sum_sliding_window/meta.ts
@@ -0,0 +1,7 @@
+export const meta: TemplateMetaData = {
+    name: "Max Subarray Sum Sliding Window",
+    id: "problems/max_subarray_sum_sliding_window",
+    category: "sliding_window",
+    difficulty: "medium",
+    disabled: false,
+};
