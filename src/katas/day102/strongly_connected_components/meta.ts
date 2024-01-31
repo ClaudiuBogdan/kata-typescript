5a6c94352efbcb782c7863d530a88f7312568589
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2024-01-31 11:08:54 +0200

    feat(day102): remove unsolved problems

--- src/katas/day102/strongly_connected_components/meta.ts
+++
@@ -1,7 +0,0 @@
-export const meta: TemplateMetaData = {
-    name: "Strongly Connected Components",
-    id: "graph/strongly_connected_components",
-    category: "graph",
-    difficulty: "medium",
-    disabled: false,
-};
