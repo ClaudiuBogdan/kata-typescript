6d8a14c8cec23e21fab66d85ae53f2a33709b52c
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2024-02-16 15:46:27 +0200

    feat(111): solve bubble sort problem

--- src/katas/day111/bubble_sort/index.ts
+++ src/katas/day111/bubble_sort/index.ts
@@ -18,6 +18,15 @@
  * @example bubbleSort([8, 3, 5, 1, 4, 2]) = [1, 2, 3, 4, 5, 8]
  */
 
-const bubbleSort = (arr: number[]): number[] => {};
+const bubbleSort = (arr: number[]): number[] => {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+            }
+        }
+    }
+    return arr;
+};
 
 export default bubbleSort;
