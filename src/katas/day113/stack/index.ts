b7b32d0f96b8e45f4a846dc1e83e9b2c3083d697
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2024-02-20 15:16:34 +0200

    feat(113): solve stack problem

--- src/katas/day113/stack/index.ts
+++ src/katas/day113/stack/index.ts
@@ -1,18 +1,41 @@
 export default class Stack<T> {
-    public length: number;
+    private head: Node<T> | null;
+    private len: number;
 
-    
+    public get length(): number {
+        return this.len;
+    }
 
     constructor() {
+        this.head = null;
+        this.len = 0;
     }
 
     push(item: T): void {
+        const node: Node<T> = {
+            value: item,
+            next: this.head,
+        };
+        this.head = node;
+        this.len++;
+    }
 
-}
     pop(): T | undefined {
+        if (this.len === 0) {
+            return;
+        }
+        const oldHead = this.head!;
+        this.head = oldHead.next;
+        this.len--;
+        return oldHead.value;
+    }
 
-}
     peek(): T | undefined {
+        return this.head?.value;
+    }
+}
 
-}
-}
+type Node<T> = {
+    value: T;
+    next: Node<T> | null;
+};
