be770ed613798fac85beaf0c7899826eb24fa5b9
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2024-02-20 16:50:42 +0200

    feat(113): solve graph list bfs problem

--- src/katas/day113/graph_list_bfs/index.ts
+++ src/katas/day113/graph_list_bfs/index.ts
@@ -5,6 +5,45 @@
  * @param source 
  * @param target 
  */
-export default function bfs(graph: WeightedAdjacencyList, source: number, target: number): number[] | null {
-  
+export default function bfs(
+    graph: WeightedAdjacencyList,
+    source: number,
+    target: number,
+): number[] | null {
+    const seen: boolean[] = new Array(graph.length).fill(false);
+    const prev: number[] = new Array(graph.length).fill(-1);
+    const queue: number[] = [];
+
+    queue.push(source);
+
+    while (queue.length > 0) {
+        const node = queue.shift()!;
+        seen[node] = true;
+
+        if (node === target) {
+            return getPath(prev, target);
+        }
+
+        for (const edge of graph[node]) {
+            if (seen[edge.to]) {
+                continue;
+            }
+            queue.push(edge.to);
+            prev[edge.to] = node;
+        }
+    }
+
+    return null;
+}
+
+function getPath(prev: number[], target: number): number[] {
+    const path: number[] = [];
+    let node = target;
+
+    while (node !== -1) {
+        path.push(node);
+        node = prev[node];
+    }
+
+    return path.reverse();
 }
