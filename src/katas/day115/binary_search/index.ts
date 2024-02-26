4646563468c4e77e01909d63f0efbc6db789c51c
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2024-02-26 12:36:43 +0200

    feat(115): solve binary search problem

--- src/katas/day115/binary_search/index.ts
+++ src/katas/day115/binary_search/index.ts
@@ -7,7 +7,7 @@
  * It repeatedly halves the portion of the list that could contain the item,
  * until you've narrowed down the possible indices to just one.
  *
- * @param {number[]} array - sorted list of numbers
+ * @param {number[]} arr - sorted list of numbers
  * @param {number} target - target number to search for
  * @return {number} - index of the target number in the list, or -1 if not found
  * @see [BinarySearch](https://www.geeksforgeeks.org/binary-search/)
@@ -15,8 +15,25 @@
  * @example binarySearch([4,5,6], 2) => -1
  */
 
-const binarySearch = (array: number[], target: number): number => {
-    
+const binarySearch = (arr: number[], target: number): number => {
+    return bs(arr, 0, arr.length - 1, target);
 };
+
+function bs(arr: number[], lo: number, hi: number, target: number): number {
+    if (lo > hi) {
+        return -1;
+    }
+
+    const mid = lo + Math.floor((hi - lo) / 2);
+    if (arr[mid] === target) {
+        return mid;
+    }
+
+    if (target < arr[mid]) {
+        return bs(arr, lo, mid - 1, target);
+    } else {
+        return bs(arr, mid + 1, hi, target);
+    }
+}
 
 export default binarySearch;
