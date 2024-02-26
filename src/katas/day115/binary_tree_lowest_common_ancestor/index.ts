6ec18638f1fb77218da0b9ca8e90c697141fcaa9
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2024-02-26 12:39:23 +0200

    feat(115): solve binary tree lowest common ancestor problem

--- src/katas/day115/binary_tree_lowest_common_ancestor/index.ts
+++ src/katas/day115/binary_tree_lowest_common_ancestor/index.ts
@@ -31,5 +31,18 @@ export function lowestCommonAncestor(
     p: TreeNode,
     q: TreeNode,
 ): TreeNode | null {
-    // Function implementation here
+    if (root === null) {
+        return null;
+    }
+
+    if (root === q || root === p) {
+        return root;
+    }
+    const left = lowestCommonAncestor(root.left, p, q);
+    const right = lowestCommonAncestor(root.right, p, q);
+
+    if (left && right) {
+        return root;
+    }
+    return left || right;
 }
