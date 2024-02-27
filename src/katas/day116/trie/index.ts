a97a134a36c10514e5e0d0210c768128c6494e0c
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2024-02-27 18:16:42 +0200

    feat(116): solve trie problem

--- src/katas/day116/trie/index.ts
+++ src/katas/day116/trie/index.ts
@@ -6,16 +6,101 @@ import { ITrie } from "./types";
  * @link https://www.geeksforgeeks.org/introduction-to-trie-data-structure-and-algorithm-tutorials/
  */
 export default class Trie implements ITrie {
+    private root: Node;
+
+    constructor() {
+        this.root = {
+            char: "",
+            children: new Map(),
+            parent: null,
+            terminal: true,
+        };
+    }
+
     insert(word: string): void {
-        throw new Error("Method not implemented.");
+        let node = this.root;
+
+        for (const char of word) {
+            if (node.children.has(char)) {
+                node = node.children.get(char)!;
+            } else {
+                const newNode: Node = {
+                    char,
+                    children: new Map(),
+                    parent: node,
+                    terminal: false,
+                };
+                node.children.set(char, newNode);
+                node = newNode;
+            }
+        }
+        node.terminal = true;
     }
+
     delete(word: string): void {
-        throw new Error("Method not implemented.");
+        let node = this.getLastNode(word);
+        if (!node || !node.terminal) {
+            return;
+        }
+
+        node.terminal = false;
+
+        if (node.children.size > 0) {
+            return;
+        }
+
+        while (!node.terminal) {
+            const parent: Node = node.parent!;
+            parent.children.delete(node.char);
+            node = parent;
+        }
     }
+
     search(word: string): boolean {
-        throw new Error("Method not implemented.");
+        const node = this.getLastNode(word);
+        return !!node && node.terminal;
     }
+
     startsWith(prefix: string): string[] {
-        throw new Error("Method not implemented.");
+        const node = this.getLastNode(prefix);
+        return this.startsWithAux(node, prefix, []);
+    }
+
+    private getLastNode(word: string): Node | null {
+        let node = this.root;
+
+        for (const char of word) {
+            const child = node.children.get(char) || null;
+            if (!child) {
+                return null;
+            }
+            node = child;
+        }
+        return node;
+    }
+
+    private startsWithAux(
+        root: Node | null,
+        prefix: string,
+        results: string[],
+    ): string[] {
+        if (!root) {
+            return results;
+        }
+        if (root.terminal) {
+            results.push(prefix);
+        }
+        for (const child of root.children.values()) {
+            this.startsWithAux(child, prefix + child.char, results);
+        }
+
+        return results;
     }
 }
+
+type Node = {
+    char: string;
+    children: Map<string, Node>;
+    parent: Node | null;
+    terminal: boolean;
+};
