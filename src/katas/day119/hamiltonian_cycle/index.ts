ad8cf977dd80322923f027dd6056acb1bacc9bcb
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2024-03-04 19:31:55 +0200

    feat(119): solve hamiltonian cycle problem

--- src/katas/day119/hamiltonian_cycle/index.ts
+++ src/katas/day119/hamiltonian_cycle/index.ts
@@ -19,6 +19,67 @@ type Graph = number[][];
  * ];
  * hamiltonianCycle(graph);  // returns [0, 1, 2, 4, 3, 0]
  */
+const source = 0;
+
 export function findHamiltonianCycle(graph: Graph): number[] | null {
-    
+    if (graph.length === 1) {
+        return [0, 0];
+    }
+
+    const seen: boolean[] = new Array(graph.length).fill(false);
+    const path: number[] = new Array(graph.length).fill(-1);
+    const pos = 0;
+
+    path[source] = pos;
+
+    if (findHamiltonianPathUtils(seen, path, graph, pos + 1)) {
+        return path.concat(0);
+    }
+
+    return null;
+}
+
+function findHamiltonianPathUtils(
+    seen: boolean[],
+    path: number[],
+    graph: Graph,
+    pos: number,
+): boolean {
+    if (pos === graph.length) {
+        return graph[path[pos - 1]][source] === 1;
+    }
+
+    for (let v = 1; v < graph.length; v++) {
+        if (isSafe(seen, path, graph, pos, v)) {
+            path[pos] = v;
+            seen[v] = true;
+
+            if (findHamiltonianPathUtils(seen, path, graph, pos + 1)) {
+                return true;
+            }
+
+            path[pos] = -1;
+            seen[v] = false;
+        }
+    }
+
+    return false;
+}
+
+function isSafe(
+    seen: boolean[],
+    path: number[],
+    graph: Graph,
+    pos: number,
+    v: number,
+) {
+    if (seen[v]) {
+        return false;
+    }
+
+    if (graph[path[pos - 1]][v] === 0) {
+        return false;
+    }
+
+    return true;
 }
