9d4e2d85633afa8ef7b1ba32be7a3a9c82fec010
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2024-03-04 19:03:24 +0200

    feat(119): solve graph list dfs problem

--- src/katas/day119/graph_list_dfs/index.ts
+++ src/katas/day119/graph_list_dfs/index.ts
@@ -7,5 +7,29 @@
  * @param needle 
  */
 export default function dfs(graph: WeightedAdjacencyList, source: number, target: number): number[] | null {
+    const seen: boolean[] = new Array(graph.length).fill(false);
+    const stack: number[] = [];
 
+    const search = (node: number): boolean => {
+        seen[node] = true;
+
+        if (node === target) {
+            stack.push(node);
+            return true;
+        }
+
+        for (const edge of graph[node]) {
+            if (seen[edge.to]) {
+                continue;
+            }
+            const found = search(edge.to);
+            if (found) {
+                stack.push(node);
+                return true;
+            }
+        }
+        return false;
+    };
+
+    return search(source) ? stack.reverse() : null;
 }
