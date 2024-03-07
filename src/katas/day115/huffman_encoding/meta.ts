8ace9d486a61d0f76ff256b2e3efdda1dbe0b856
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2024-03-07 13:35:26 +0200

    feat: remove unsolved problems

--- src/katas/day115/huffman_encoding/meta.ts
+++
@@ -1,7 +0,0 @@
-export const meta: TemplateMetaData = {
-    name: "Huffman Encoding",
-    id: "greedy/huffman_encoding",
-    category: "greedy",
-    difficulty: "hard",
-    disabled: false,
-};
