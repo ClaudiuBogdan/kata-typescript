5a7b07b00e03bbc59c721aacfadcba70239a7db0
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2024-03-07 13:36:02 +0200

    feat(120): generate day 120

---
+++ src/katas/day120/prims_list/index.ts
@@ -0,0 +1,10 @@
+/**
+ * @function Prims - Prim's algorithm for Minimum Spanning Tree
+ * @description Prim's algorithm is a minimum spanning tree algorithm that takes a graph as input and finds the subset of the edges of that graph which form a tree that includes every vertex, and has the minimum sum of weights among all the trees that can be formed from the graph
+ * @link https://www.programiz.com/dsa/prim-algorithm
+ * @link https://www.geeksforgeeks.org/prims-minimum-spanning-tree-mst-greedy-algo-5/
+ * @param list 
+ */
+export default function prims(list: WeightedAdjacencyList): WeightedAdjacencyList | null {
+
+}
