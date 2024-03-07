702318bc47a08e52a6fbeb456a0696daf3407a74
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2024-03-07 13:52:27 +0200

    feat(120): solve quick sort problem

--- src/katas/day120/quick_sort/index.ts
+++ src/katas/day120/quick_sort/index.ts
@@ -18,7 +18,36 @@
  */
 
 const quickSort = (arr: number[]): number[] => {
-
+    return qs(arr, 0, arr.length - 1);
 };
+
+function qs(arr: number[], lo: number, hi: number): number[] {
+    if (lo >= hi) {
+        return arr;
+    }
+
+    const pivotIdx = partition(arr, lo, hi);
+
+    qs(arr, lo, pivotIdx - 1);
+    qs(arr, pivotIdx + 1, hi);
+
+    return arr;
+}
+
+function partition(arr: number[], lo: number, hi: number): number {
+    const partitionVal = arr[hi];
+    let idx = lo - 1;
+
+    for (let i = lo; i < hi; i++) {
+        if (arr[i] <= partitionVal) {
+            idx++;
+            [arr[i], arr[idx]] = [arr[idx], arr[i]];
+        }
+    }
+
+    idx++;
+    [arr[idx], arr[hi]] = [arr[hi], arr[idx]];
+    return idx;
+}
 
 export default quickSort;
